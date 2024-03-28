8cfe36e6e7d94f7bbac4635e3bca57ce609a8034
Anwaar Khalid <shahanwaar.khalid8@gmail.com>
2024-03-28 08:07:03 +0000

    renamed some files

---
+++ src/unify/Exceptions.py
@@ -0,0 +1,41 @@
+class UnifyError(Exception):
+    pass
+
+class KeyError(UnifyError):
+    pass
+
+class BadRequestError(UnifyError):
+    pass
+
+class AuthenticationError(UnifyError):
+    pass
+
+class PermissionDeniedError(UnifyError):
+    pass
+
+class NotFoundError(UnifyError):
+    pass
+
+class ConflictError(UnifyError):
+    pass
+
+class UnprocessableEntityError(UnifyError):
+    pass
+
+class RateLimitError(UnifyError):
+    pass
+
+class InternalServerError(UnifyError):
+    pass
+
+
+status_error_map = {
+    400: BadRequestError,
+    401: AuthenticationError,
+    403: PermissionDeniedError,
+    404: NotFoundError,
+    409: ConflictError,
+    422: UnprocessableEntityError,
+    429: RateLimitError,
+    500: InternalServerError,
+}
