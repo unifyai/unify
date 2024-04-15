340a30c401fb73768542013ccc30e3804474e22c
Anwaar Khalid <shahanwaar.khalid8@gmail.com>
2024-04-15 01:21:55 +0000

    minor fix

--- unify/utils.py
+++ unify/utils.py
@@ -1,7 +1,7 @@
 import os
 from typing import Optional, Tuple
 
-from unifyai.exceptions import UnifyError
+from unify.exceptions import UnifyError
 
 _available_dynamic_modes = [
     "lowest-input-cost",
