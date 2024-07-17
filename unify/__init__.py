49a69ce5f5a7f019d9ef75c843b40932c40c617b
guillesanbri <Guillesanbri@hotmail.com>
2024-07-17 13:47:28 +0000

    add functions to upload and evaluate datasets

--- unify/__init__.py
+++ unify/__init__.py
@@ -2,4 +2,16 @@
 
 from unify.chat import ChatBot  # noqa: F403
 from unify.clients import AsyncUnify, Unify  # noqa: F403
-from unify.utils import list_endpoints, list_models, list_providers
+from unify.utils import (
+    list_endpoints, 
+    list_models, 
+    list_providers, 
+    upload_dataset_from_file,
+    upload_dataset_from_dictionary,
+    delete_dataset,
+    download_dataset,
+    list_datasets,
+    evaluate,
+    delete_evaluation,
+    list_evaluations,
+)
