09d2f59c01a9432b81ad9035378adb019d8659d2
Daniel Lenton <daniel.lenton11@imperial.ac.uk>
2024-07-31 00:16:49 +0100

    added get_credits function.

--- unify/utils.py
+++ unify/utils.py
@@ -383,3 +383,30 @@ def list_evaluations(
     if response.status_code != 200:
         raise ValueError(response.text)
     return _res_to_list(response)
+
+
+def get_credits(
+    api_key: Optional[str] = None
+) -> float:
+    """
+    Returns the credits remaining in the user account, in USD.
+
+    Args:
+        api_key (str): If specified, unify API key to be used. Defaults
+        to the value in the `UNIFY_KEY` environment variable.
+
+    Returns:
+        float: The credits remaining in USD.
+    Raises:
+        ValueError: If there was an HTTP error.
+    """
+    api_key = _validate_api_key(api_key)
+    headers = {
+        "accept": "application/json",
+        "Authorization": f"Bearer {api_key}",
+    }
+    # Send GET request to the /get_credits endpoint
+    response = requests.get(_base_url + "/get_credits", headers=headers)
+    if response.status_code != 200:
+        raise ValueError(response.text)
+    return _res_to_list(response)["credits"]
