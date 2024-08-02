083487b6bfdd494f6f05f5d97945fd49ce379a07
Daniel Lenton <daniel.lenton11@imperial.ac.uk>
2024-08-02 16:14:57 +0100

    added OpenAI tool use unit test.

---
+++ tests/queries/test_function_calling.py
@@ -0,0 +1,74 @@
+import os
+import json
+import unittest
+from openai.types.chat.chat_completion_message_tool_call import Function
+from openai.types.chat.chat_completion_message import ChatCompletionMessage
+from openai.types.chat.chat_completion_message_tool_call import ChatCompletionMessageToolCall
+
+from unify.clients import Unify
+
+
+class TestUnifyFunctionCalling(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.valid_api_key = os.environ.get("UNIFY_KEY")
+
+    def test_openai_function_calling(self) -> None:
+        # adapted from: https://cookbook.openai.com/examples/how_to_call_functions_with_chat_models
+        tools = [
+            {
+                "type": "function",
+                "function": {
+                    "name": "get_n_day_weather_forecast",
+                    "description": "Get an N-day weather forecast",
+                    "parameters": {
+                        "type": "object",
+                        "properties": {
+                            "location": {
+                                "type": "string",
+                                "description": "The city and state, e.g. San Francisco, CA",
+                            },
+                            "format": {
+                                "type": "string",
+                                "enum": ["celsius", "fahrenheit"],
+                                "description": "The temperature unit to use. Infer this from the users location.",
+                            },
+                            "num_days": {
+                                "type": "integer",
+                                "description": "The number of days to forecast",
+                            }
+                        },
+                        "required": ["location", "format", "num_days"]
+                    },
+                }
+            },
+        ]
+
+        client = Unify(
+            api_key=self.valid_api_key, endpoint="gpt-4o@openai"
+        )
+        result = client.generate(
+            user_prompt="What is the weather going to be like in Glasgow, Scotland over the next 5 days?",
+            tool_choice="required",
+            tools=tools,
+            message_content_only=False)
+        message = result.choices[0].message
+        self.assertIsInstance(message, ChatCompletionMessage)
+        tool_calls = message.tool_calls
+        self.assertIsInstance(tool_calls, list)
+        self.assertEqual(len(tool_calls), 1)
+        tool_call = tool_calls[0]
+        self.assertIsInstance(tool_call, ChatCompletionMessageToolCall)
+        function = tool_call.function
+        self.assertIsInstance(function, Function)
+        arguments = function.arguments
+        self.assertIsInstance(arguments, str)
+        arguments = json.loads(arguments)
+        self.assertIsInstance(arguments, dict)
+        self.assertIn("location", arguments)
+        self.assertIn("format", arguments)
+        self.assertIn("num_days", arguments)
+
+
+if __name__ == "__main__":
+    unittest.main()
