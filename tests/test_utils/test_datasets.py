ff63414a825963b42da8200a58f491b2fddb1938
Daniel <daniel.lenton11@imperial.ac.uk>
2024-09-04 18:03:56 +0100

    updated datasets to use the new format, where queries are stored, rather than prompts.

--- tests/test_utils/test_datasets.py
+++ tests/test_utils/test_datasets.py
@@ -6,6 +6,7 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 
 
 class TestDatasets(unittest.TestCase):
+
     def test_upload_and_delete_dataset_from_file(self) -> None:
         if "TestUploadAndDelete" in unify.list_datasets():
             unify.delete_dataset("TestUploadAndDelete")
@@ -19,15 +20,33 @@ class TestDatasets(unittest.TestCase):
     def test_upload_and_delete_dataset_from_dict(self) -> None:
         prompts = [
             {
-                "prompt": "This is the first prompt",
+                "query": {
+                    "messages":
+                        [{
+                            "role": "user",
+                            "content": "This is the first prompt"
+                        }]
+                },
                 "ref_answer": "First reference answer",
             },
             {
-                "prompt": "This is the second prompt",
+                "query": {
+                    "messages":
+                        [{
+                            "role": "user",
+                            "content": "This is the second prompt"
+                        }]
+                },
                 "ref_answer": "Second reference answer",
             },
             {
-                "prompt": "This is the third prompt",
+                "query": {
+                    "messages":
+                        [{
+                            "role": "user",
+                            "content": "This is the third prompt"
+                        }]
+                },
                 "ref_answer": "Third reference answer",
             },
         ]
