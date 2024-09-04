456a15d22eca03983ec3b85b93f1053f3a04c61e
Daniel <daniel.lenton11@imperial.ac.uk>
2024-09-04 10:28:12 +0100

    added default arguments to the Unify client, such that any aspect of the queries can be set in the constructor or via setter methods, which are then passed to each generate function if no other overriding arguments are passed.

--- unify/utils/helpers.py
+++ unify/utils/helpers.py
@@ -1,7 +1,7 @@
 import os
 import json
 import requests
-from typing import List, Dict, Optional, Union
+from typing import List, Dict, Optional, Union, Any
 
 
 def _res_to_list(response: requests.Response) -> Union[List, Dict]:
@@ -16,3 +16,7 @@ def _validate_api_key(api_key: Optional[str]) -> str:
             "UNIFY_KEY is missing. Please make sure it is set correctly!",
         )
     return api_key
+
+
+def _default(value: Any, default_value: Any) -> Any:
+    return value if value is not None else default_value
