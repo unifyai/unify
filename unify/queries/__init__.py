3bc1f9c9713a139f780edd31630d1fe703997034
Daniel <daniel.lenton11@imperial.ac.uk>
2024-09-04 12:50:57 +0100

    removed errenous commans from Query pydantic model, which were causing the default values to all be tuples.

--- unify/queries/__init__.py
+++ unify/queries/__init__.py
@@ -12,23 +12,23 @@ from openai.types.chat.completion_create_params import ResponseFormat
 
 
 class Query(BaseModel):
-    messages: Optional[List[ChatCompletionMessageParam]] = None,
-    frequency_penalty: Optional[float] = None,
-    logit_bias: Optional[Dict[str, int]] = None,
-    logprobs: Optional[bool] = None,
-    top_logprobs: Optional[int] = None,
-    max_tokens: Optional[int] = 1024,
-    n: Optional[int] = None,
-    presence_penalty: Optional[float] = None,
-    response_format: Optional[ResponseFormat] = None,
-    seed: Optional[int] = None,
-    stop: Union[Optional[str], List[str]] = None,
-    temperature: Optional[float] = 1.0,
-    top_p: Optional[float] = None,
-    tools: Optional[List[ChatCompletionToolParam]] = None,
-    tool_choice: Optional[ChatCompletionToolChoiceOptionParam] = None,
-    parallel_tool_calls: Optional[bool] = None,
-    # extra_headers: Optional[OpenAIHeaders] = None,  # ToDo: fix Omit error
-    extra_headers: Optional[Mapping[str, str]] = None,
-    extra_query: Optional[OpenAIQuery] = None,
+    messages: Optional[List[ChatCompletionMessageParam]] = None
+    frequency_penalty: Optional[float] = None
+    logit_bias: Optional[Dict[str, int]] = None
+    logprobs: Optional[bool] = None
+    top_logprobs: Optional[int] = None
+    max_tokens: Optional[int] = 1024
+    n: Optional[int] = None
+    presence_penalty: Optional[float] = None
+    response_format: Optional[ResponseFormat] = None
+    seed: Optional[int] = None
+    stop: Union[Optional[str], List[str]] = None
+    temperature: Optional[float] = 1.0
+    top_p: Optional[float] = None
+    tools: Optional[List[ChatCompletionToolParam]] = None
+    tool_choice: Optional[ChatCompletionToolChoiceOptionParam] = None
+    parallel_tool_calls: Optional[bool] = None
+    # extra_headers: Optional[OpenAIHeaders] = None  # ToDo: fix Omit error
+    extra_headers: Optional[Mapping[str, str]] = None
+    extra_query: Optional[OpenAIQuery] = None
     extra_body: Optional[OpenAIBody] = None
