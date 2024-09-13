4c81b7f4e65b2950ffccf9ff93f1e8ae52fb5ef4
tje <tje541@gmail.com>
2024-09-13 20:03:35 +0100

    progress

--- tests/test_utils/test_datasets.py
+++ tests/test_utils/test_datasets.py
@@ -21,31 +21,25 @@ class TestDatasets(unittest.TestCase):
         entries = [
             {
                 "prompt": {
-                    "messages":
-                        [{
-                            "role": "user",
-                            "content": "This is the first user message"
-                        }]
+                    "messages": [
+                        {"role": "user", "content": "This is the first user message"}
+                    ]
                 },
                 "ref_answer": "First reference answer",
             },
             {
                 "prompt": {
-                    "messages":
-                        [{
-                            "role": "user",
-                            "content": "This is the second user message"
-                        }]
+                    "messages": [
+                        {"role": "user", "content": "This is the second user message"}
+                    ]
                 },
                 "ref_answer": "Second reference answer",
             },
             {
                 "prompt": {
-                    "messages":
-                        [{
-                            "role": "user",
-                            "content": "This is the third user message"
-                        }]
+                    "messages": [
+                        {"role": "user", "content": "This is the third user message"}
+                    ]
                 },
                 "ref_answer": "Third reference answer",
             },
@@ -56,3 +50,46 @@ class TestDatasets(unittest.TestCase):
         assert "TestFromDict" in unify.list_datasets()
         unify.delete_dataset("TestFromDict")
         assert "TestFromDict" not in unify.list_datasets()
+
+    def test_atomic_functions(self):
+        entries = [
+            {
+                "prompt": {
+                    "messages": [
+                        {"role": "user", "content": "This is the first user message"}
+                    ]
+                },
+                "ref_answer": "First reference answer",
+            },
+            {
+                "prompt": {
+                    "messages": [
+                        {"role": "user", "content": "This is the second user message"}
+                    ]
+                },
+                "ref_answer": "Second reference answer",
+            },
+            {
+                "prompt": {
+                    "messages": [
+                        {"role": "user", "content": "This is the third user message"}
+                    ]
+                },
+                "ref_answer": "Third reference answer",
+            },
+        ]
+
+        dataset_name = "TestAtomic"
+        if dataset_name in unify.list_datasets():
+            unify.delete_dataset(dataset_name)
+        unify.upload_dataset_from_dictionary(dataset_name, entries)
+
+        new_prompt_data = {
+            "messages": [
+                {"role": "user", "content": "What is the powerhouse of the cell?"}
+            ]
+        }
+        unify.datasets.add_prompt(dataset_name, new_prompt_data)
+
+        unify.datasets.download_dataset(dataset_name)
+        unify.datasets.delete_prompt(dataset_name)
--- unify/utils/datasets.py
+++ unify/utils/datasets.py
@@ -166,3 +166,37 @@ def list_datasets(api_key: Optional[str] = None) -> List[str]:
     response = requests.get(BASE_URL + "/dataset/list", headers=headers)
     response.raise_for_status()
     return _res_to_list(response)
+
+
+
+def add_prompt(name: str, prompt_data: dict, api_key: Optional[str] = None):
+    """
+    Adds a prompt to a dataset.
+    """
+    api_key = _validate_api_key(api_key)
+    headers = {
+        "accept": "application/json",
+        "Authorization": f"Bearer {api_key}",
+    }
+    body = {"name": name, "prompt_data": prompt_data}
+    response = requests.post(
+        BASE_URL + "/dataset/add_prompt", headers=headers, json=params
+    )
+    return response.json()
+
+
+def delete_prompt(name: str, prompt_id: int, api_key: Optional[str] = None):
+    """
+    Delete a prompt from a dataset
+    """
+    api_key = _validate_api_key(api_key)
+    headers = {
+        "accept": "application/json",
+        "Authorization": f"Bearer {api_key}",
+    }
+    params = {"name": name, "prompt_id": prompt_id}
+    response = requests.delete(
+        BASE_URL + "/dataset/delete_prompt", headers=headers, params=params
+    )
+    return response.json()
+
