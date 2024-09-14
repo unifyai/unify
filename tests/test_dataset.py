0642bef00e0e7b698fc2b63396f800679b95a1df
Daniel <daniel.lenton11@imperial.ac.uk>
2024-09-15 01:22:31 +0200

    added TestDatasetCombining.

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -114,3 +114,67 @@ class TestDatasetIndexing(unittest.TestCase):
         for datum, msg in zip(dataset, msgs):
             self.assertIsInstance(datum, Datum)
             self.assertEqual(datum.prompt.messages[0]["content"], msg)
+
+
+class TestDatasetCombining(unittest.TestCase):
+
+    def test_add_datasets(self) -> None:
+        msgs = ["a", "b", "c", "d"]
+        dataset1 = unify.Dataset(msgs[0:2])
+        dataset2 = unify.Dataset(msgs[2:])
+        dataset = dataset1 + dataset2
+        self.assertEqual(len(dataset), len(msgs))
+        for datum, msg in zip(dataset, msgs):
+            self.assertEqual(datum.prompt.messages[0]["content"], msg)
+
+    def test_add_datasets_w_duplicates(self) -> None:
+        msgs1 = ["a", "b"]
+        msgs2 = ["b", "c"]
+        dataset1 = unify.Dataset(msgs1)
+        dataset2 = unify.Dataset(msgs2)
+        dataset = dataset1 + dataset2
+        self.assertEqual(len(dataset), 3)
+        for datum, msg in zip(dataset, ("a", "b", "c")):
+            self.assertEqual(datum.prompt.messages[0]["content"], msg)
+
+    def test_dataset_inplace_addition(self) -> None:
+        msgs = ["a", "b", "c", "d"]
+        dataset = unify.Dataset(msgs[0:2])
+        did = id(dataset)
+        dataset2 = unify.Dataset(msgs[2:])
+        dataset += dataset2
+        self.assertEqual(did, id(dataset))
+        self.assertEqual(len(dataset), len(msgs))
+        for datum, msg in zip(dataset, msgs):
+            self.assertEqual(datum.prompt.messages[0]["content"], msg)
+
+    def test_dataset_single_item_addition(self) -> None:
+        dataset = unify.Dataset("a") + "b"
+        self.assertEqual(len(dataset), 2)
+        self.assertEqual(dataset[0].prompt.messages[0]["content"], "a")
+        self.assertEqual(dataset[1].prompt.messages[0]["content"], "b")
+
+    def test_dataset_reverse_addition(self) -> None:
+        dataset = "a" + unify.Dataset("b")
+        self.assertEqual(len(dataset), 2)
+        self.assertEqual(dataset[0].prompt.messages[0]["content"], "a")
+        self.assertEqual(dataset[1].prompt.messages[0]["content"], "b")
+
+    def test_dataset_from_prompt_addition(self) -> None:
+        dataset = unify.Prompt("a") + unify.Prompt("b")
+        self.assertEqual(len(dataset), 2)
+        self.assertEqual(dataset[0].prompt.messages[0]["content"], "a")
+        self.assertEqual(dataset[1].prompt.messages[0]["content"], "b")
+
+    def test_dataset_from_datum_addition(self) -> None:
+        dataset = unify.Datum("a") + unify.Datum("b")
+        self.assertEqual(len(dataset), 2)
+        self.assertEqual(dataset[0].prompt.messages[0]["content"], "a")
+        self.assertEqual(dataset[1].prompt.messages[0]["content"], "b")
+
+    def test_dataset_from_multi_item_addition(self) -> None:
+        dataset = "a" + unify.Prompt("b") + unify.Datum("c")
+        self.assertEqual(len(dataset), 3)
+        self.assertEqual(dataset[0].prompt.messages[0]["content"], "a")
+        self.assertEqual(dataset[1].prompt.messages[0]["content"], "b")
+        self.assertEqual(dataset[2].prompt.messages[0]["content"], "c")
