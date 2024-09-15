2114e18b0bbcf54d15ff9ce046d3766024094ca2
Daniel <daniel.lenton11@imperial.ac.uk>
2024-09-15 22:08:38 +0200

    updates to Agent.

--- unify/agent.py
+++ unify/agent.py
@@ -7,10 +7,13 @@ from unify.types import Prompt
 
 class Agent(abc.ABC):
 
+    def __init__(self):
+        self._call = unify.with_logging(self._call, endpoint=self.__class__.__name__)
+
     @abc.abstractmethod
     def _call(self, prompt: Union[str, Prompt]):
         raise NotImplemented
 
     @abc.abstractmethod
     def __call__(self, prompt: Union[str, Prompt]):
-        return unify.log(self._call(unify.handle_query(prompt)))
+        return self._call(**prompt.dict())
