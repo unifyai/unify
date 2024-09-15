8bb6a7b693742101aa87d758c1a8cd09feaefdf4
Daniel <daniel.lenton11@imperial.ac.uk>
2024-09-15 15:11:50 +0200

    fully documented Dataset class.

--- unify/dataset.py
+++ unify/dataset.py
@@ -17,7 +17,7 @@ class Dataset(_Formatted):
         name: str = None,
         auto_sync: Union[bool, str] = False,
         api_key: Optional[str] = None,
-    ):
+    ) -> None:
         """
         Initialize a local dataset of LLM queries.
 
@@ -72,20 +72,44 @@ class Dataset(_Formatted):
 
     @property
     def name(self) -> str:
+        """
+        Name of the dataset.
+        """
         self._auto_sync()
         return self._name
 
     @property
     def auto_sync(self) -> Union[bool, str]:
+        """
+        Whether or not auto-sync is turned on
+        """
         self._auto_sync()
         return self._auto_sync_flag
 
     def set_name(self, name: str) -> Dataset:
+        """
+        Set the name of the dataset.
+
+        Args:
+            name: The name to set the dataset to.
+
+        Returns:
+            This dataset, useful for chaining methods.
+        """
         self._auto_sync()
         self._name = name
         return self
 
     def set_auto_sync(self, auto_sync: Union[bool, str]) -> Dataset:
+        """
+        Set the value of the auto-sync flag.
+
+        Args:
+            auto_sync: The value to set the auto-sync flag to.
+
+        Returns:
+            This dataset, useful for chaining methods.
+        """
         self._auto_sync()
         self._auto_sync_flag = auto_sync
         return self
@@ -95,7 +119,7 @@ class Dataset(_Formatted):
         name: str,
         auto_sync: bool = False,
         api_key: Optional[str] = None,
-    ):
+    ) -> Dataset:
         """
         Initialize a local dataset of LLM queries, from the upstream dataset.
 
@@ -109,13 +133,16 @@ class Dataset(_Formatted):
             retrieve the API key from the environment variable UNIFY_KEY. Defaults to
             None.
 
+        Returns:
+            The dataset, with contents downloaded from upstream.
+
         Raises:
             UnifyError: If the API key is missing.
         """
         data = unify.download_dataset(name, api_key=api_key)
         return Dataset(data, name=name, auto_sync=auto_sync, api_key=api_key)
 
-    def _assert_name_exists(self):
+    def _assert_name_exists(self) -> None:
         assert self._name is not None, (
             "Dataset name must be specified in order to upload, download, sync or "
             "compare to a corresponding dataset in your upstream account. "
@@ -123,7 +150,7 @@ class Dataset(_Formatted):
             "upstream dataset, or create a new name if it doesn't yet exist upstream."
         )
 
-    def upload(self, overwrite=False) -> Dataset:
+    def upload(self, overwrite: bool = False) -> Dataset:
         """
         Uploads all unique local data in the dataset to the user account upstream.
         This function will not download any uniques from upstream.
@@ -131,7 +158,10 @@ class Dataset(_Formatted):
         Set `overwrite=True` to disregard any pre-existing upstream data.
 
         Args:
-            overwrite: Whether to overwrite the upstream dataset if it already exists
+            overwrite: Whether to overwrite the upstream dataset if it already exists.
+
+        Returns:
+            This dataset, useful for chaining methods.
         """
         self._assert_name_exists()
         dataset_exists_upstream = self._name in unify.list_datasets(self._api_key)
@@ -156,7 +186,7 @@ class Dataset(_Formatted):
             self._auto_sync_flag = auto_sync_flag
         return self
 
-    def download(self, overwrite=False) -> Dataset:
+    def download(self, overwrite: bool = False) -> Dataset:
         """
         Downloads all unique upstream data from the user account to the local dataset.
         This function will not upload any unique values stored locally.
@@ -165,6 +195,9 @@ class Dataset(_Formatted):
 
         Args:
             overwrite: Whether to overwrite the local data, if any already exists
+
+        Returns:
+            This dataset after the in-place download, useful for chaining methods.
         """
         self._assert_name_exists()
         if overwrite:
@@ -180,7 +213,7 @@ class Dataset(_Formatted):
             self._auto_sync_flag = auto_sync_flag
         return self
 
-    def _auto_sync(self):
+    def _auto_sync(self) -> None:
         if self._auto_sync_flag in (True, "both", "download_only"):
             auto_sync_flag = self._auto_sync_flag
             self._auto_sync_flag = False
@@ -196,6 +229,9 @@ class Dataset(_Formatted):
         """
         Synchronize the dataset in both directions, downloading any values missing
         locally, and uploading any values missing from upstream in the account.
+
+        Returns:
+            This dataset after the in-place sync, useful for chaining methods.
         """
         auto_sync_flag = self._auto_sync_flag
         self._auto_sync_flag = False
@@ -207,6 +243,9 @@ class Dataset(_Formatted):
     def upstream_diff(self) -> Dataset:
         """
         Prints the difference between the local dataset and the upstream dataset.
+
+        Returns:
+            This dataset after printing the diff, useful for chaining methods.
         """
         self._assert_name_exists()
         upstream_dataset = unify.download_dataset(self._name, api_key=self._api_key)
@@ -224,26 +263,32 @@ class Dataset(_Formatted):
         return self
 
     def add(self, other: Union[Dataset, str, Dict, Prompt, Datum,
-                               List[Union[str, Dict, Prompt, Datum]]]):
+                               List[Union[str, Dict, Prompt, Datum]]]) -> Dataset:
         """
         Adds another dataset to this one, return a new Dataset instance, with this
         new dataset receiving all unique queries from the other added dataset.
 
         Args:
             other: The other dataset being added to this one.
+
+        Returns:
+            The new dataset following the addition.
         """
         other = other if isinstance(other, Dataset) else Dataset(other)
         data = list(dict.fromkeys(self._data + other._data))
         return Dataset(data=data, auto_sync=self._auto_sync_flag, api_key=self._api_key)
 
     def sub(self, other: Union[Dataset, str, Dict, Prompt, Datum,
-                               List[Union[str, Dict, Prompt, Datum]]]):
+                               List[Union[str, Dict, Prompt, Datum]]]) -> Dataset:
         """
         Subtracts another dataset from this one, return a new Dataset instance, with
         this new dataset losing all queries from the other subtracted dataset.
 
         Args:
             other: The other dataset being added to this one.
+
+        Returns:
+            The new dataset following the subtraction.
         """
         other = other if isinstance(other, Dataset) else Dataset(other)
         assert other in self, (
@@ -254,13 +299,16 @@ class Dataset(_Formatted):
         return Dataset(data=data, auto_sync=self._auto_sync_flag, api_key=self._api_key)
 
     def __iadd__(self, other: Union[Dataset, str, Dict, Prompt, Datum,
-                                    List[Union[str, Dict, Prompt, Datum]]]):
+                                    List[Union[str, Dict, Prompt, Datum]]]) -> Dataset:
         """
         Adds another dataset to this one, with this dataset receiving all unique queries
         from the other added dataset.
 
         Args:
             other: The other dataset being added to this one.
+
+        Returns:
+            This dataset following the in-place addition.
         """
         other = other if isinstance(other, Dataset) else Dataset(other)
         self._data = list(dict.fromkeys(self._data + other._data))
@@ -268,13 +316,16 @@ class Dataset(_Formatted):
         return self
 
     def __isub__(self, other: Union[Dataset, str, Dict, Prompt, Datum,
-                                    List[Union[str, Dict, Prompt, Datum]]]):
+                                    List[Union[str, Dict, Prompt, Datum]]]) -> Dataset:
         """
         Subtracts another dataset from this one, with this dataset losing all queries
         from the other subtracted dataset.
 
         Args:
             other: The other dataset being added to this one.
+
+        Returns:
+            This dataset following the in-place subtraction.
         """
         other = other if isinstance(other, Dataset) else Dataset(other)
         assert other in self, (
@@ -286,36 +337,118 @@ class Dataset(_Formatted):
         return self
 
     def __add__(self, other: Union[Dataset, str, Dict, Prompt, Datum,
-                                   List[Union[str, Dict, Prompt, Datum]]]):
+                                   List[Union[str, Dict, Prompt, Datum]]]) -> Dataset:
+        """
+        Adds another dataset to this one via the + operator, return a new Dataset
+        instance, with this new dataset receiving all unique queries from the other
+        added dataset.
+
+        Args:
+            other: The other dataset being added to this one.
+
+        Returns:
+            The new dataset following the addition.
+        """
         return self.add(other)
 
     def __radd__(self, other: Union[Dataset, str, Dict, Prompt, Datum,
-                                    List[Union[str, Dict, Prompt, Datum]]]):
+                                    List[Union[str, Dict, Prompt, Datum]]]) -> Dataset:
+        """
+        Adds another dataset to this one via the + operator, this is used if the
+        other item does not have a valid __add__ method for these two types. Return a
+        new Dataset instance, with this new dataset receiving all unique queries from
+        the other added dataset.
+
+        Args:
+            other: The other dataset being added to this one.
+
+        Returns:
+            The new dataset following the addition.
+        """
         return Dataset(other).add(self)
 
     def __sub__(self, other: Union[Dataset, str, Dict, Prompt, Datum,
-                                   List[Union[str, Dict, Prompt, Datum]]]):
+                                   List[Union[str, Dict, Prompt, Datum]]]) -> Dataset:
+        """
+        Subtracts another dataset from this one via the - operator, return a new Dataset
+        instance, with this new dataset losing all queries from the other subtracted
+        dataset.
+
+        Args:
+            other: The other dataset being subtracted from this one.
+
+        Returns:
+            The new dataset following the subtraction.
+        """
         return self.sub(other)
 
     def __rsub__(self, other: Union[Dataset, str, Dict, Prompt, Datum,
-                                    List[Union[str, Dict, Prompt, Datum]]]):
+                                    List[Union[str, Dict, Prompt, Datum]]]) -> Dataset:
+        """
+        Subtracts another dataset from this one via the - operator, this is used if the
+        other item does not have a valid __sub__ method for these two types. Return a
+        new Dataset instance, with this new dataset losing all queries from the other
+        subtracted dataset.
+
+        Args:
+            other: The other dataset being subtracted from this one.
+
+        Returns:
+            The new dataset following the subtraction.
+        """
         return Dataset(other).sub(self)
 
-    def __iter__(self):
+    def __iter__(self) -> Datum:
+        """
+        Iterates through the dataset, return one Datum instance at a time.
+
+        Returns:
+            A Datum instance per iteration.
+        """
         self._auto_sync()
         for x in self._data:
             yield x
 
-    def __contains__(self, item):
+    def __contains__(self, item: Union[Dataset, str, Dict, Prompt, Datum,
+                                       List[Union[str, Dict, Prompt, Datum]]]) -> bool:
+        """
+        Determine whether the item is contained within the dataset. The item is cast to
+        a Dataset instance, and can therefore take on many different types. Only returns
+        True if *all* entries in the passed dataset are contained within this dataset.
+
+        Args:
+            item: The item to cast to a Dataset before checking if it's a subset of this
+            one.
+
+        Returns:
+            Boolean, as to whether or not the passed Dataset is a subset of this one.
+        """
         item = item if isinstance(item, Dataset) else Dataset(item)
         this_set = set(self._data)
         combined_set = set(self._data + item._data)
         return len(this_set) == len(combined_set)
 
-    def __len__(self):
+    def __len__(self) -> int:
+        """
+        Returns the number of entries contained within the dataset.
+
+        Returns:
+            The number of entries in the dataset.
+        """
         return len(self._data)
 
-    def __getitem__(self, item):
+    def __getitem__(self, item: Union[int, slice]) -> Union[Datum, Dataset]:
+        """
+        Gets an item from the dataset, either via an int or slice. In the case of an
+        int then a Datum instance is returned, and for a slice a Dataset instance is
+        returned.
+
+        Args:
+            item: integer or slice for extraction.
+
+        Returns:
+            A Datum or Dataset instance, for int and slice queries respectively.
+        """
         self._auto_sync()
         if isinstance(item, int):
             return self._data[item]
@@ -324,6 +457,9 @@ class Dataset(_Formatted):
         raise TypeError("expected item to be of type int or slice,"
                         "but found {} of type {}".format(item, type(item)))
 
-    def __rich_repr__(self):
+    def __rich_repr__(self) -> List[Datum]:
+        """
+        Used by the rich package for representing and print the instance.
+        """
         self._auto_sync()
         yield self._data
