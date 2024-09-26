99116f89b73f1fcb54c253ee943817c2d23f5208
Daniel Lenton <daniel.lenton11@imperial.ac.uk>
2024-09-26 15:16:53 +0100

    removed abstract _call method from Agent class.

--- unify/agent.py
+++ unify/agent.py
@@ -8,12 +8,10 @@ from unify.types import Prompt
 class Agent(abc.ABC):
 
     def __init__(self):
-        self._call = unify.with_logging(self._call, endpoint=self.__class__.__name__)
-
-    @abc.abstractmethod
-    def _call(self, prompt: Union[str, Prompt]):
-        raise NotImplemented
+        self.__call__ = unify.with_logging(
+            self.__call__, endpoint=self.__class__.__name__
+        )
 
     @abc.abstractmethod
     def __call__(self, prompt: Union[str, Prompt]):
-        return self._call(**prompt.dict())
+        raise NotImplemented
