26ba22f028e4935440ac1e9ab9178375e9f8c008
Daniel Lenton <daniel.lenton11@imperial.ac.uk>
2024-09-26 16:25:47 +0100

    split chat types into seperate modules, chat_completion and prompt.

---
+++ unify/types/chat/chat_completion.py
@@ -0,0 +1,64 @@
+from pydantic import ConfigDict
+from typing import Optional, List
+from openai.types.chat import ChatCompletion as _ChatCompletion
+from openai.types.completion_usage import CompletionUsage as _CompletionUsage
+from openai.types.chat.chat_completion import \
+    ChatCompletionMessage as _ChatCompletionMessage, Choice as _Choice
+
+from ..base import _FormattedBaseModel
+
+
+class ChatCompletionMessage(_FormattedBaseModel, _ChatCompletionMessage):
+    model_config = ConfigDict(extra="forbid")
+
+
+class Choice(_FormattedBaseModel, _Choice):
+    model_config = ConfigDict(extra="forbid")
+    # only override pydantic types  which require FormattedBaseModel
+    message: ChatCompletionMessage
+
+
+class CompletionUsage(_FormattedBaseModel, _CompletionUsage):
+    model_config = ConfigDict(extra="forbid")
+    # cost is an extra field we've added, not in the OpenAI standard
+    cost: float
+
+
+class ChatCompletion(_FormattedBaseModel, _ChatCompletion):
+    model_config = ConfigDict(extra="forbid")
+    # only override pydantic types  which require FormattedBaseModel
+    choices: List[Choice]
+    usage: Optional[CompletionUsage] = None
+
+    def __init__(self, assistant_message: Optional[str] = None, **kwargs):
+        """
+        Create ChatCompletion instance.
+
+        Args:
+            assistant_message: The assistant message, optional.
+
+        Returns:
+            The pydantic ChatCompletion instance.
+        """
+        if assistant_message:
+            kwargs = {
+                **dict(
+                    id="",
+                    choices=[
+                        Choice(
+                            finish_reason="stop",
+                            index=0,
+                            message=ChatCompletionMessage(
+                                role="assistant",
+                                content=assistant_message
+                            )
+                        )
+                    ],
+                    created=0,
+                    model="",
+                    object="chat.completion",
+                    **kwargs
+                ),
+                **kwargs
+            }
+        super().__init__(**kwargs)
