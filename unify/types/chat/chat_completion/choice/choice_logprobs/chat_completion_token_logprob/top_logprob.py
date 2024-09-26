665f0fd5376027a22bc05f3b79562dc0bc8096ce
Daniel Lenton <daniel.lenton11@imperial.ac.uk>
2024-09-26 17:51:43 +0100

    removed top_logprob.py file.

--- unify/types/chat/chat_completion/choice/choice_logprobs/chat_completion_token_logprob/top_logprob.py
+++
@@ -1,8 +0,0 @@
-from pydantic import ConfigDict
-from openai.types.chat.chat_completion_token_logprob import TopLogprob as _TopLogprob
-
-from ......base import _FormattedBaseModel
-
-
-class TopLogprob(_FormattedBaseModel, _TopLogprob):
-    model_config = ConfigDict(extra="forbid")
