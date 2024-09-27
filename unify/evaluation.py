a12a46b3199d8e4ac30835c3b2ded4f2b721feda
Daniel Lenton <daniel.lenton11@imperial.ac.uk>
2024-09-27 18:47:02 +0100

    added EvaluationSet for storing collections of Evaluation instances.

--- unify/evaluation.py
+++ unify/evaluation.py
@@ -1,7 +1,8 @@
 from pydantic import Extra
-from typing import Union, Optional
+from typing import Union, Optional, List
 
 from unify.agent import Agent
+from unify.dataset import Dataset
 from unify.chat.clients import _Client
 from unify.types import Prompt, Score, Datum, ChatCompletion
 
@@ -12,3 +13,21 @@ class Evaluation(Datum, extra=Extra.allow, arbitrary_types_allowed=True):
     agent: Union[str, _Client, Agent]
     score: Score
     rationale: Optional[str]
+
+
+class EvaluationSet(Dataset):
+
+    def __init__(
+            self,
+            data: Union[Evaluation, List[Evaluation]],
+            *,
+            name: str = None,
+            auto_sync: Union[bool, str] = False,
+            api_key: Optional[str] = None,
+    ) -> None:
+        super().__init__(
+            data=data,
+            name=name,
+            auto_sync=auto_sync,
+            api_key=api_key
+        )
