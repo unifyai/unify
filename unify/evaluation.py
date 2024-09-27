d77a5c0d09f66eb17ed1d5272c1c1db02f859b1f
Daniel Lenton <daniel.lenton11@imperial.ac.uk>
2024-09-27 19:56:40 +0100

    added LLMJuryEvaluationSet, for returning a set of Evaluations from each judge within an LLM Jury.

--- unify/evaluation.py
+++ unify/evaluation.py
@@ -69,3 +69,32 @@ class EvaluationSet(Dataset):
     @property
     def score_distribution(self) -> Dict[float, int]:
         return self._score_distribution
+
+
+class LLMJuryEvaluationSet(EvaluationSet):
+
+    def __init__(
+            self,
+            evaluations: Union[Evaluation, List[Evaluation]],
+            *,
+            name: str = None,
+            auto_sync: Union[bool, str] = False,
+            api_key: Optional[str] = None
+    ) -> None:
+        self._judge_score_distribution =\
+            {
+                k: [e.evaluator.name for e in evaluations]
+                for k in self._class_config.keys()
+            }
+        super().__init__(
+            evaluations=evaluations,
+            name=name,
+            auto_sync=auto_sync,
+            api_key=api_key
+        )
+
+    # Properties
+
+    @property
+    def judge_score_distribution(self) -> Dict[float, List[str]]:
+        return self._judge_score_distribution
