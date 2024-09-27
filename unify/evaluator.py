ecaed3a8fced4b4e2f5a85e3772dca38937bbd15
Daniel Lenton <daniel.lenton11@imperial.ac.uk>
2024-09-27 01:28:45 +0100

    made a start on LLMJudge testing.

--- unify/evaluator.py
+++ unify/evaluator.py
@@ -136,3 +136,111 @@ class Evaluator(abc.ABC):
             rationale=rationale,
             **kwargs
         )
+
+
+class LLMJudge(Evaluator, abc.ABC):
+
+    def __init__(
+            self,
+            client: _UniLLMClient,
+            judge_prompt: Union[str, Prompt],
+            name: Optional[str] = None,
+            prompt_parser: Optional[Dict[str, List[Union[str, int]]]] = None,
+            response_parser: Optional[Dict[str, List[Union[str, int]]]] = None,
+            extra_parser: Optional[Dict[str, List[Union[str, int]]]] = None,
+    ):
+        """
+        Creates an LLM as a Judge Evaluator.
+
+        Args:
+            client: The client to use as the LLM Judge.
+
+            judge_prompt: The prompt for the judge to use when performing evaluations.
+
+            name: The name to give to this LLM Judge evaluator, optional.
+
+            prompt_parser: Function to parse the prompt and update corresponding
+            placeholders in the judge user message and system message, optional.
+
+            response_parser: Function to parse the response and update corresponding
+            placeholders in the judge user message and system message, optional.
+
+            extra_parser: Function to parse the extra fields provided alongside the
+            prompt, and update corresponding placeholders in the judge user message and
+            system message, optional.
+        """
+        self._client = client
+        self._judge_prompt = cast(judge_prompt, Prompt)
+        if prompt_parser is None:
+            self._prompt_parser = {"user_message": ["messages", -1, "content"]}
+        else:
+            self._prompt_parser = prompt_parser
+        if response_parser is None:
+            self._response_parser = {
+                "assistant_response": ["choices", 0, "message", "content"]
+            }
+        else:
+            self._response_parser = response_parser
+        self._extra_parser = extra_parser
+        self._class_config_parser = {"class_config": None}
+        super().__init__(name)
+
+    @staticmethod
+    def _parse(item, parse_rule):
+        for idx in parse_rule:
+            if isinstance(idx, int):
+                if not isinstance(item, list) or len(item) < idx:
+                    return
+            elif not isinstance(item, dict) or idx not in item:
+                return
+            item = item[idx]
+        return json.dumps(item) if isinstance(item, dict) else str(item)
+
+    def _update_judge_messages(
+            self,
+            item: Union[Prompt, ChatCompletion, Dict],
+            parser: Optional[Dict[str, List[Union[str, int]]]],
+            messages: List
+    ):
+        if parser is None or item in (None, {}):
+            return messages
+        for key, parse_rule in parser.items():
+            if parse_rule is None:
+                content = json.dumps(item) if isinstance(item, dict) else str(item)
+            else:
+                content = self._parse(
+                    item if isinstance(item, dict) else item.model_dump(), parse_rule
+                )
+            print("item: {}".format(item))
+            print("parse_rule: {}".format(parse_rule))
+            print("content: {}".format(content))
+            messages = [
+                {k: (v.replace("{" + key + "}", content) if k == "content" else v)
+                 for k, v in msg.items()} for msg in messages
+            ]
+        return messages
+
+    def _evaluate(
+            self,
+            prompt: Prompt,
+            response: ChatCompletion,
+            **kwargs
+    ) -> Tuple[Union[bool, float, Score], Optional[str]]:
+        messages = copy.deepcopy(self._judge_prompt.messages)
+        for i, (item, parser) in enumerate(zip(
+                (prompt, response, kwargs, self.class_config),
+                (self._prompt_parser, self._response_parser, self._extra_parser,
+                 self._class_config_parser)
+        )):
+            messages = self._update_judge_messages(
+                copy.deepcopy(item),
+                parser,
+                messages
+            )
+        kw = self._judge_prompt.model_dump()
+        kw["messages"] = messages
+        judge_response = self._client.generate(**kw)
+        judge_message = judge_response.choices[0].message.content
+        judge_score = judge_message.split("\n")[-1]
+        return float("".join(c for c in judge_score if c.isdigit() or c == "."))
+        d = 0
