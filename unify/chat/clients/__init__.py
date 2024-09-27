c26303b936c25a172de18aa2c949ec7b937b08fc
Daniel Lenton <daniel.lenton11@imperial.ac.uk>
2024-09-27 11:16:23 +0100

    renamed MultiLLMAsync to AsyncMultiLLM, and added sync and async converter methods.

--- unify/chat/clients/__init__.py
+++ unify/chat/clients/__init__.py
@@ -3,4 +3,4 @@ from .base import _Client
 from . import uni_llm
 from .uni_llm import _UniLLMClient, Unify, AsyncUnify
 from . import multi_llm
-from .multi_llm import _MultiLLMClient, MultiLLM, MultiLLMAsync
+from .multi_llm import _MultiLLMClient, MultiLLM, AsyncMultiLLM
