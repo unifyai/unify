098133c7d251163562c3041ca0b632a4b9a072a7
Daniel Lenton <daniel.lenton11@imperial.ac.uk>
2024-09-28 12:14:39 +0100

    fix circular import error.

--- unify/types/evaluation.py
+++ unify/types/evaluation.py
@@ -34,27 +34,3 @@ class Score(_FormattedBaseModel, abc.ABC):
     @abc.abstractmethod
     def config(self) -> Dict[float, str]:
         raise NotImplementedError
-
-
-class ScoreSet(unify.Dataset):
-
-    def __init__(
-            self,
-            scores: Union[Score, List[Score]],
-            *,
-            name: str = None,
-            auto_sync: Union[bool, str] = False,
-            api_key: Optional[str] = None
-    ) -> None:
-        if not isinstance(scores, list):
-            scores = [scores]
-        assert all(type(s) is type(scores[0]) for s in scores), \
-            "All scores passed to a ScoreSet must be of the same type."
-        self._class_config = scores[0].config
-
-        super().__init__(
-            data=scores,
-            name=name,
-            auto_sync=auto_sync,
-            api_key=api_key
-        )
