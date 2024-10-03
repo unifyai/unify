b84f53197f01c680ba6fa612b17366a40053d418
Daniel Lenton <daniel.lenton11@imperial.ac.uk>
2024-10-03 20:57:40 +0100

    Revert "python class repr cleanup (#45)"
    This reverts commit c36d4ffd471aebc18ff57b5bc5c4211d452617ec.

--- unify/types/dataset.py
+++ unify/types/dataset.py
@@ -1,5 +1,5 @@
 from pydantic import Extra
-from typing import List, Literal, Optional, Union
+from typing import Optional
 
 import unify
 from .base import _FormattedBaseModel
@@ -46,10 +46,3 @@ class Datum(_FormattedBaseModel, extra=Extra.allow):
 
     def __hash__(self):
         return hash(str(self))
-
-
-class Dataset(_FormattedBaseModel, extra=Extra.allow):
-    prompts: List[Union[Datum, Literal["..."]]] # type: ignore
-
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
