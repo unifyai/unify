b4abf868ee36d2dc655949dd984bf36913a23e99
Daniel <daniel.lenton11@imperial.ac.uk>
2024-10-08 22:00:00 +0300

    fixed broken imports.

--- unify/casting.py
+++ unify/casting.py
@@ -1,5 +1,5 @@
 from typing import Union, Type, List
-from unify.types import Prompt, ChatCompletion, Score
+from unify.types import Prompt, ChatCompletion
 
 
 # Upcasting
@@ -16,14 +16,6 @@ def _bool_to_float(boolean: bool) -> float:
     return float(boolean)
 
 
-def _float_to_score(float_in: float, score_type: Type[Score]) -> Score:
-    return score_type(float_in)
-
-
-def _bool_to_score(boolean: bool, score_type: Type[Score]) -> Score:
-    return _float_to_score(_bool_to_float(boolean), score_type)
-
-
 # Downcasting
 
 def _prompt_to_usr_msg(prompt: Prompt) -> str:
@@ -36,14 +28,6 @@ def _chat_completion_to_assis_msg(chat_completion: ChatCompletion) -> str:
 
 def _float_to_bool(float_in: float) -> bool:
     return bool(float_in)
-
-
-def _score_to_float(score: Score) -> float:
-    return score.score[0]
-
-
-def _score_to_bool(score: Score) -> bool:
-    return _float_to_bool(_score_to_float(score))
 
 
 # Cast Dict
@@ -61,26 +45,17 @@ _CAST_DICT = {
     },
     bool: {
         float: _bool_to_float,
-        Score: _bool_to_score
     },
     float: {
         bool: _float_to_bool,
-        Score: _float_to_score
     },
-    type(None): {
-        Score: _float_to_score
-    },
-    Score: {
-        bool: _score_to_bool,
-        float: _score_to_float
-    }
 }
 
 
 def _cast_from_selection(
-        inp: Union[str, bool, float, Score, Prompt, ChatCompletion],
-        targets: List[Union[float, Score, Prompt, ChatCompletion]]
-) -> Union[str, bool, float, Score, Prompt, ChatCompletion]:
+        inp: Union[str, bool, float, Prompt, ChatCompletion],
+        targets: List[Union[float, Prompt, ChatCompletion]]
+) -> Union[str, bool, float, Prompt, ChatCompletion]:
     """
     Upcasts the input if possible, based on the permitted upcasting targets provided.
 
@@ -100,20 +75,18 @@ def _cast_from_selection(
     targets = [target for target in targets if target in cast_fns]
     assert len(targets) == 1, "There must be exactly one valid casting target."
     to_type = targets[0]
-    if issubclass(to_type, Score):
-        return cast_fns[to_type](inp, to_type)
     return cast_fns[to_type](inp)
 
 
 # Public function
 
 def cast(
-        inp: Union[str, bool, float, Score, Prompt, ChatCompletion],
+        inp: Union[str, bool, float, Prompt, ChatCompletion],
         to_type: Union[
-            Type[Union[str, bool, float, Score, Prompt, ChatCompletion]],
-            List[Type[Union[str, bool, float, Score, Prompt, ChatCompletion]]]
+            Type[Union[str, bool, float, Prompt, ChatCompletion]],
+            List[Type[Union[str, bool, float, Prompt, ChatCompletion]]]
         ],
-) -> Union[str, bool, float, Score, Prompt, ChatCompletion]:
+) -> Union[str, bool, float, Prompt, ChatCompletion]:
     """
     Cast the input to the specified type.
 
@@ -130,18 +103,16 @@ def cast(
     input_type = type(inp)
     if input_type is to_type:
         return inp
-    if issubclass(to_type, Score):
-        return _CAST_DICT[input_type][Score](inp, to_type)
     return _CAST_DICT[input_type][to_type](inp)
 
 
 def try_cast(
-        inp: Union[str, bool, float, Score, Prompt, ChatCompletion],
+        inp: Union[str, bool, float, Prompt, ChatCompletion],
         to_type: Union[
-            Type[Union[str, bool, float, Score, Prompt, ChatCompletion]],
-            List[Type[Union[str, bool, float, Score, Prompt, ChatCompletion]]]
+            Type[Union[str, bool, float, Prompt, ChatCompletion]],
+            List[Type[Union[str, bool, float, Prompt, ChatCompletion]]]
         ],
-) -> Union[str, bool, float, Score, Prompt, ChatCompletion]:
+) -> Union[str, bool, float, Prompt, ChatCompletion]:
     # noinspection PyBroadException
     try:
         return cast(inp, to_type)
