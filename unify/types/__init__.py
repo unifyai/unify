867dd3ae99c5cc56fb157842eb497aabb818945e
Daniel <daniel.lenton11@imperial.ac.uk>
2024-10-08 22:00:00 +0300

    fixed broken imports.

--- unify/types/__init__.py
+++ unify/types/__init__.py
@@ -1,8 +1,6 @@
 from .base import _Formatted, _FormattedBaseModel
 from .base import *
 from .chat import *
-from .evaluation import *
 
 from . import base
 from . import chat
-from . import evaluation
