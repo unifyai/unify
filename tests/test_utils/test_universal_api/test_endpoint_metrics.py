4dbfecfc6874de416787b786463e1c5e0d241b3c
Daniel <daniel.lenton11@imperial.ac.uk>
2024-10-13 00:43:50 +0300

    improved endpoint metric testing.

--- tests/test_utils/test_universal_api/test_endpoint_metrics.py
+++ tests/test_utils/test_universal_api/test_endpoint_metrics.py
@@ -1,12 +1,52 @@
 import unittest
 
 import unify
-from unify import Metrics
+from unify import Metrics, delete_custom_api_key
+
+
+# noinspection PyBroadException
+class CustomEndpointHandler:
+
+    def __init__(self, endpoint_name, endpoint_url, key_name, key_value):
+        self._endpoint_name = endpoint_name
+        self._endpoint_url = endpoint_url
+        self._key_name = key_name
+        self._key_value = key_value
+
+    def _cleanup(self):
+        try:
+            unify.delete_custom_api_key(self._key_name)
+        except:
+            pass
+        try:
+            unify.delete_custom_endpoint(self._endpoint_name)
+        except:
+            pass
+
+    def __enter__(self):
+        self._cleanup()
+        unify.create_custom_api_key(self._key_name, self._key_value)
+        unify.create_custom_endpoint(
+            self._endpoint_name, self._endpoint_url, self._key_name
+        )
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self._cleanup()
 
 
 class TestEndpointMetrics(unittest.TestCase):
 
-    def test_get_endpoint_metrics(self):
+    def setUp(self):
+        self._endpoint_name = "test_new_endpoint"
+        self._endpoint_url = "test.com"
+        self._key_name = "test_key"
+        self._key_value = "4321"
+
+        self._handler = CustomEndpointHandler(
+            self._endpoint_name, self._endpoint_url, self._key_name, self._key_value
+        )
+
+    def test_get_public_endpoint_metrics(self):
         metrics = unify.get_endpoint_metrics("gpt-4o@openai")
         self.assertIsInstance(metrics, Metrics)
         self.assertTrue(hasattr(metrics, "time_to_first_token"))
