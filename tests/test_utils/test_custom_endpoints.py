236c98462e66f72a5609467d47f6058e5f393557
Daniel <daniel.lenton11@imperial.ac.uk>
2024-10-12 20:24:13 +0300

    modularized custom endpoint testing.

--- tests/test_utils/test_custom_endpoints.py
+++ tests/test_utils/test_custom_endpoints.py
@@ -3,35 +3,77 @@ import unittest
 import unify
 
 
+class CustomEndpointHandler:
+
+    def __init__(self, key_name, endpoint_names):
+        self._key_name = key_name
+        self._endpoint_names = endpoint_names
+
+    def _handle(self):
+        if self._key_name in unify.list_custom_api_keys():
+            unify.delete_custom_api_key(self._key_name)
+        for endpoint_name in self._endpoint_names:
+            if endpoint_name in [ep["name"] for ep in unify.list_custom_endpoints()]:
+                unify.delete_custom_endpoint(endpoint_name)
+
+    def __enter__(self):
+        self._handle()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self._handle()
+
+
 class TestCustomEndpoints(unittest.TestCase):
+
     def setUp(self):
         self.endpoint_name = "test_new_endpoint"
         self.new_endpoint_name = "renamed_test_new_endpoint"
         self.endpoint_url = "test.com"
         self.key_name = "test_key"
         self.key_value = "4321"
-        unify.utils.custom_api_keys.create_custom_api_key(self.key_name, self.key_value)
-
-    def test_custom_endpoints(self):
-        unify.utils.custom_endpoints.create_custom_endpoint(
-            self.endpoint_name, self.endpoint_url, self.key_name
+        unify.create_custom_api_key(self.key_name, self.key_value)
+        self._custom_endpoint_handler = CustomEndpointHandler(
+            self.key_name, [self.endpoint_name, self.new_endpoint_name]
         )
-        unify.utils.custom_endpoints.list_custom_endpoints()
-        unify.utils.custom_endpoints.rename_custom_endpoint(
-            self.endpoint_name, self.new_endpoint_name
-        )
-        unify.utils.custom_endpoints.delete_custom_endpoint(self.new_endpoint_name)
 
-    def tearDown(self):
-        try:
-            unify.utils.custom_api_keys.delete_custom_api_key(self.key_name)
-        except:
-            pass
-        try:
-            unify.utils.custom_api_keys.delete_custom_endpoint(self.endpoint_name)
-        except:
-            pass
-        try:
-            unify.utils.custom_api_keys.delete_custom_endpoint(self.new_endpoint_name)
-        except:
-            pass
+    def test_create_custom_endpoint(self):
+        with self._custom_endpoint_handler:
+            unify.create_custom_endpoint(
+                self.endpoint_name, self.endpoint_url, self.key_name
+            )
+
+    def test_list_custom_endpoints(self):
+        with self._custom_endpoint_handler:
+            unify.create_custom_endpoint(
+                self.endpoint_name, self.endpoint_url, self.key_name
+            )
+            custom_endpoints = unify.list_custom_endpoints()
+            assert len(custom_endpoints) == 1
+            assert self.endpoint_name == custom_endpoints[0]["name"]
+
+    def test_rename_custom_endpoint(self):
+        with self._custom_endpoint_handler:
+            unify.create_custom_endpoint(
+                self.endpoint_name, self.endpoint_url, self.key_name
+            )
+            custom_endpoints = unify.list_custom_endpoints()
+            assert len(custom_endpoints) == 1
+            assert self.endpoint_name == custom_endpoints[0]["name"]
+            unify.rename_custom_endpoint(
+                self.endpoint_name, self.new_endpoint_name
+            )
+            custom_endpoints = unify.list_custom_endpoints()
+            assert len(custom_endpoints) == 1
+            assert self.new_endpoint_name == custom_endpoints[0]["name"]
+
+    def test_delete_custom_endpoints(self):
+        with self._custom_endpoint_handler:
+            unify.create_custom_endpoint(
+                self.endpoint_name, self.endpoint_url, self.key_name
+            )
+            custom_endpoints = unify.list_custom_endpoints()
+            assert len(custom_endpoints) == 1
+            assert self.endpoint_name == custom_endpoints[0]["name"]
+            unify.delete_custom_endpoint(self.endpoint_name)
+            custom_endpoints = unify.list_custom_endpoints()
+            assert len(custom_endpoints) == 0
