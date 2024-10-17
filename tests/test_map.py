0bbf48e9ac09598603b676e9b480104fc6990ccf
Daniel <daniel.lenton11@imperial.ac.uk>
2024-10-17 20:47:21 +0300

    added failing test_asyncio_map.

--- tests/test_map.py
+++ tests/test_map.py
@@ -27,7 +27,7 @@ class TestMap(unittest.TestCase):
 
     def setUp(self):
         self._client = unify.Unify("gpt-4o@openai")
-
+        self._async_client = unify.AsyncUnify("gpt-4o@openai")
         self._qs = ["3 - 1", "4 + 7", "6 + 2", "9 - 3", "7 + 9"]
 
     @staticmethod
@@ -43,6 +43,10 @@ class TestMap(unittest.TestCase):
 
     def _evaluate(self, q: str):
         response = self._client.generate(q)
+        self._evaluate_response(q, response)
+
+    def _evaluate_w_log(self, q: str):
+        response = self._client.generate(q)
         score = self._evaluate_response(q, response)
         return unify.log(
             question=q,
@@ -51,14 +55,28 @@ class TestMap(unittest.TestCase):
             skip_duplicates=False,
         )
 
+    async def _async_evaluate(self, q: str):
+        response = await self._async_client.generate(q)
+        return self._evaluate_response(q, response)
+
     def test_threaded_map(self) -> None:
         with ProjectHandling():
             with unify.Project("test_project"):
                 t0 = time.perf_counter()
-                unify.map(self._evaluate, self._qs)
+                unify.map(self._evaluate_w_log, self._qs)
                 mapped_time = time.perf_counter() - t0
                 t0 = time.perf_counter()
                 for q in self._qs:
-                    self._evaluate(q)
+                    self._evaluate_w_log(q)
                 serial_time = time.perf_counter() - t0
-                assert serial_time > 2 * mapped_time  # at least than 4x faster
+                assert serial_time > 2 * mapped_time  # at least than 2x faster
+
+    def test_asyncio_map(self) -> None:
+        t0 = time.perf_counter()
+        unify.map(self._async_evaluate, self._qs, mode="asyncio")
+        mapped_time = time.perf_counter() - t0
+        t0 = time.perf_counter()
+        for q in self._qs:
+            self._evaluate(q)
+        serial_time = time.perf_counter() - t0
+        assert serial_time > 2 * mapped_time  # at least than 2x faster
