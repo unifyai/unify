b372d66da515b5c1e58962ccafe838e15afebcb5
Daniel Lenton <daniel.lenton11@imperial.ac.uk>
2024-10-22 23:49:24 +0100

    added versioned function, Versioned class, a simple test for these, and removed version argument from logging.

--- tests/test_utils/test_evals/test_versioning.py
+++ tests/test_utils/test_evals/test_versioning.py
@@ -16,3 +16,16 @@ class TestVersioning(unittest.TestCase):
             == "    @staticmethod\n    def my_func():\n        a = 1 + 2\n        "
             "b = a + 3\n        return b\n"
         )
+
+    def test_versioned(self):
+        system_prompt = unify.versioned("you are an expert")
+        assert system_prompt.version == 0
+        assert system_prompt.value == "you are an expert"
+        system_prompt.update("you are an expert mathematician")
+        assert system_prompt.version == 1
+        assert system_prompt.value == "you are an expert mathematician"
+        assert len(system_prompt) == 2
+        assert system_prompt.at_version(0).value == "you are an expert"
+        assert system_prompt.value == "you are an expert mathematician"
+        system_prompt.set_version(0)
+        assert system_prompt.value == "you are an expert"
