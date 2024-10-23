2a2038dbe3559a4914e318b436dceb5b4dfae4cd
Daniel Lenton <daniel.lenton11@imperial.ac.uk>
2024-10-23 03:06:54 +0100

    added lock to make caching thread safe.

--- unify/_caching.py
+++ unify/_caching.py
@@ -1,5 +1,6 @@
 import json
 import os
+import threading
 from typing import Dict, Optional, Union, Any
 
 from unify.types import ChatCompletion
@@ -10,28 +11,38 @@ _cache_dir = (
 )
 _cache_fpath: str = os.path.join(_cache_dir, ".cache.json")
 
+CACHE_LOCK = threading.Lock()
+
 
 def _create_cache_if_none():
     global _cache
+    global CACHE_LOCK
+    CACHE_LOCK.acquire()
     if _cache is None:
         if not os.path.exists(_cache_fpath):
             with open(_cache_fpath, "w") as outfile:
                 json.dump({}, outfile)
         with open(_cache_fpath) as outfile:
             _cache = json.load(outfile)
+    CACHE_LOCK.release()
 
 
 # noinspection PyTypeChecker,PyUnboundLocalVariable
 def _get_cache(kw: Dict[str, Any]) -> Union[None, Dict]:
+    global CACHE_LOCK
+    CACHE_LOCK.acquire()
     _create_cache_if_none()
     kw = {k: v for k, v in kw.items() if v is not None}
     kw_str = json.dumps(kw)
     if kw_str in _cache:
         return ChatCompletion(**json.loads(_cache[kw_str]))
+    CACHE_LOCK.release()
 
 
 # noinspection PyTypeChecker,PyUnresolvedReferences
 def _write_to_cache(kw, response):
+    global CACHE_LOCK
+    CACHE_LOCK.acquire()
     _create_cache_if_none()
     kw = {k: v for k, v in kw.items() if v is not None}
     kw_str = json.dumps(kw)
@@ -39,3 +50,4 @@ def _write_to_cache(kw, response):
     _cache[kw_str] = response_str
     with open(_cache_fpath, "w") as outfile:
         json.dump(_cache, outfile)
+    CACHE_LOCK.release()
