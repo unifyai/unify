5a925721f0551e9f2b1a223dd1d2a3ddbe5aa6de
Daniel Lenton <daniel.lenton11@imperial.ac.uk>
2024-10-23 01:05:39 +0100

    added test_versioned_from_upstream.

--- tests/test_utils/test_evals/test_versioning.py
+++ tests/test_utils/test_evals/test_versioning.py
@@ -29,3 +29,22 @@ class TestVersioning(unittest.TestCase):
         assert system_prompt.value == "you are an expert mathematician"
         system_prompt.set_version(0)
         assert system_prompt.value == "you are an expert"
+
+    def test_versioned_from_upstream(self):
+        project = "my_project"
+        if project in unify.list_projects():
+            unify.delete_project(project)
+        unify.create_project(project)
+        unify.activate(project)
+        unify.log(system_prompt=unify.versioned("you are an expert", 0))
+        system_prompt = unify.Versioned.from_upstream("system_prompt")
+        assert system_prompt.version == 0
+        assert system_prompt.value == "you are an expert"
+        unify.log(system_prompt=unify.versioned("you are an expert mathematician", 1))
+        assert len(system_prompt) == 1
+        system_prompt.download()
+        assert len(system_prompt) == 2
+        assert system_prompt.at_version(0).value == "you are an expert"
+        assert system_prompt.value == "you are an expert mathematician"
+        system_prompt.set_version(0)
+        assert system_prompt.value == "you are an expert"
