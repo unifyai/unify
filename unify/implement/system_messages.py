cfa9a9e4e47edcb1f0fa9aacb08c64ab88bdf09f
Daniel Lenton <daniel.lenton11@imperial.ac.uk>
2024-10-25 16:35:57 +0100

    updated system messages, such that there is no indent (making code replacements possible), and also added dashes to indicate code sections.

--- unify/implement/system_messages.py
+++ unify/implement/system_messages.py
@@ -1,87 +1,88 @@
 CODING_SYS_MESSAGE_BASE = """
-    You are encouraged to make use of imaginary functions whenever you don't have enough
-    context to solve the task fully, or if you believe a modular solution would be best.
-    If that's the case, then make sure to give the function an expressive name, like so:
+You are encouraged to make use of imaginary functions whenever you don't have enough
+context to solve the task fully, or if you believe a modular solution would be best.
+If that's the case, then make sure to give the function an expressive name, like so:
 
-    companies = get_all_companies_from_crm()
-    large_companies = filter_companies_based_on_headcount(
-        companies, headcount=100, greater_than=True
-    )
+companies = get_all_companies_from_crm()
+large_companies = filter_companies_based_on_headcount(
+    companies, headcount=100, greater_than=True
+)
 
-    Please DO NOT implement any inner functions. Just assume they exist, and make calls
-    to them like the examples above.
+Please DO NOT implement any inner functions. Just assume they exist, and make calls
+to them like the examples above.
 
-    As the very last part of your response, please add the full implementation with
-    correct indentation and valid syntax, starting with any necessary module imports
-    (if relevant), and then the full function implementation, for example:
+As the very last part of your response, please add the full implementation with
+correct indentation and valid syntax, starting with any necessary module imports
+(if relevant), and then the full function implementation, for example:
 
-    import {some_module}
-    from {another_module} import {function}
+import {some_module}
+from {another_module} import {function}
 
-    def {name}{signature}:
-        {implementation}
+def {name}{signature}:
+    {implementation}
 """
 INIT_CODING_SYS_MESSAGE = """
-    You should write a Python implementation for a function {name} with the
-    following signature and docstring:
+You should write a Python implementation for a function {name} with the
+following signature and docstring:
 
-    {signature}
+{signature}
 
-    {docstring}
+{docstring}
 
     """
 UPDATING_CODING_SYS_MESSAGE = """
-    You should *update* the Python implementation (preserving the function name,
-    arguments, and general structure), and only make changes as requested by the user
-    in the chat.
+You should *update* the Python implementation (preserving the function name,
+arguments, and general structure), and only make changes as requested by the user
+in the chat.
 
 """
 
 DOCSTRING_SYS_MESSAGE_HEAD = """
-    We need to implement a new function {name}, but before we implement it we first need
-    to  decide on exactly how it should behave.
+We need to implement a new function `{name}`, but before we implement it we first
+need to  decide on exactly how it should behave.
 """
 
 DOCSTRING_SYS_MESSAGE_FIRST_CONTEXT = """
-    To help us, we know that the function {child_name} is called inside the function
-    {parent_name}, which has the following implementation:
+To help us, we know that the function `{child_name}` is called inside the function
+`{parent_name}`, which has the following implementation:
 
-    {parent_implementation}
+```python
+{parent_implementation}
+```
 
-    Specifically, the line where {child_name} is called is:
-
-    {calling_line}
+Specifically, the line where `{child_name}` is called is: `{calling_line}`.
 """
 
 DOCSTRING_SYS_MESSAGE_EXTRA_CONTEXT = """
-    This function ({child_name}) is itself called inside the function {parent_name},
-    which has the following implementation:
+This function (`{child_name}`) is itself called inside the function `{parent_name}`,
+which has the following implementation:
 
-    {parent_implementation}
+```python
+{parent_implementation}
+```
 
-    Specifically, the line where {child_name} is called is:
-
-    {calling_line}
+Specifically, the line where `{child_name}` is called is: `{calling_line}`.
 """
 
 DOCSTRING_SYS_MESSAGE_TAIL = """
-    Given all of this context, your task is to provide a well informed proposal for the
-    docstring and argument specification (with typing) for the new function {name},
-    in the following format:
+Given all of this context, your task is to provide a well informed proposal for the
+docstring and argument specification (with typing) for the new function {name},
+in the following format:
 
-    def {name}({arg1}: {type1}, {arg2}: {type2} = {default2}, ...):
-        \"\"\"
-        {A very thorough description for exactly what this function needs to do.}
+```python
+def {name}({arg1}: {type1}, {arg2}: {type2} = {default2}, ...):
+    \"\"\"
+    {A very thorough description for exactly what this function needs to do.}
 
-        Args:
-            {arg1}: {arg1 description}
+    Args:
+        {arg1}: {arg1 description}
 
-            {arg2}: {arg2 description}
+        {arg2}: {arg2 description}
 
-        Returns:
-            {return description}
-        \"\"\"
+    Returns:
+        {return description}
+    \"\"\"
+```
 
-
-    Please respond in the format as above, and write nothing else after your answer.
+Please respond in the format as above, and write nothing else after your answer.
 """
