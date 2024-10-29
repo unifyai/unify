9947a25be6e1a575552e4340ec952adead1340cf
Daniel Lenton <daniel.lenton11@imperial.ac.uk>
2024-10-29 23:43:31 +0000

    removed test_anthropic_function_calling.

--- tests/test_universal_api/test_tool_use.py
+++ tests/test_universal_api/test_tool_use.py
@@ -77,50 +77,6 @@ class TestUnifyToolUse(unittest.TestCase):
         self.assertIn("format", arguments)
         self.assertIn("num_days", arguments)
 
-    @unittest.skip("known problem, on backlog to fix")
-    def test_anthropic_function_calling(self) -> None:
-        # adapted from: https://cookbook.openai.com/examples/how_to_call_functions_with_chat_models and # noqa
-        # https://docs.anthropic.com/en/docs/build-with-claude/tool-use#single-tool-example
-        tools = [
-            {
-                "name": "get_n_day_weather_forecast",
-                "description": "Get an N-day weather forecast",
-                "input_schema": {
-                    "type": "object",
-                    "properties": {
-                        "location": {
-                            "type": "string",
-                            "description": "The city and state, e.g. San Francisco, CA",
-                        },
-                        "format": {
-                            "type": "string",
-                            "enum": ["celsius", "fahrenheit"],
-                            "description": (
-                                "The temperature unit to use. Infer this from "
-                                "the users location."
-                            ),
-                        },
-                        "num_days": {
-                            "type": "integer",
-                            "description": "The number of days to forecast",
-                        },
-                    },
-                    "required": ["location", "format", "num_days"],
-                },
-            },
-        ]
-
-        client = Unify(api_key=self.valid_api_key, endpoint="claude-3-opus@anthropic")
-        client.generate(
-            user_message=(
-                "What is the weather going to be like in Glasgow, Scotland"
-                "over the next 5 days?"
-            ),
-            tool_choice={"type": "any"},
-            tools=tools,
-            return_full_completion=True,
-        )
-
 
 if __name__ == "__main__":
     unittest.main()
