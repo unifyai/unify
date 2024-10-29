090d92ccf08fd1d9f0d94b63cf5b11277fbf0dbc
Daniel Lenton <daniel.lenton11@imperial.ac.uk>
2024-10-29 22:43:48 +0000

    fixed test_utils/test_logging.py.

--- tests/test_evals/test_utils/test_logging.py
+++ tests/test_evals/test_utils/test_logging.py
@@ -15,20 +15,20 @@ class TestLogging(unittest.TestCase):
             "system_prompt": "You are a weather assistant",
             "user_prompt": "hello world",
         }
-        assert len(unify.get_logs(project)) == 0
-        log_id = unify.log(project, **data).id
-        project_logs = unify.get_logs(project)
+        assert len(unify.get_logs(project=project)) == 0
+        log_id = unify.log(project=project, **data).id
+        project_logs = unify.get_logs(project=project)
         assert len(project_logs) and project_logs[0].id == log_id
         id_log = unify.get_log_by_id(log_id)
         assert len(id_log) and "user_prompt" in id_log.entries
-        unify.delete_log_entries("user_prompt", log_id)
+        unify.delete_log_entries(entry="user_prompt", logs=log_id)
         id_log = unify.get_log_by_id(log_id)
         assert len(id_log) and "user_prompt" not in id_log.entries
-        unify.add_log_entries(log_id, user_prompt=data["user_prompt"])
+        unify.add_log_entries(logs=log_id, user_prompt=data["user_prompt"])
         id_log = unify.get_log_by_id(log_id)
         assert len(id_log) and "user_prompt" in id_log.entries
-        unify.delete_logs(log_id)
-        assert len(unify.get_logs(project)) == 0
+        unify.delete_logs(logs=log_id)
+        assert len(unify.get_logs(project=project)) == 0
         try:
             unify.get_log_by_id(log_id)
             assert False
@@ -40,8 +40,11 @@ class TestLogging(unittest.TestCase):
         if project in unify.list_projects():
             unify.delete_project(project)
         unify.create_project(project)
-        unify.log(project, dataset=unify.Dataset(["a", "b", "c"], name="letters"))
-        logs = unify.get_logs(project)
+        unify.log(
+            project=project,
+            dataset=unify.Dataset(["a", "b", "c"], name="letters"),
+        )
+        logs = unify.get_logs(project=project)
         assert len(logs) == 1
         assert logs[0].entries == {"dataset": "letters"}
         downloaded = unify.download_dataset("letters")
@@ -58,13 +61,13 @@ class TestLogging(unittest.TestCase):
             "system_prompt": "You are a weather assistant",
             "user_prompt": "hello world",
         }
-        assert len(unify.get_logs(project)) == 0
-        log0 = unify.log(project, **data)
-        log1 = unify.log(project, **data)
+        assert len(unify.get_logs(project=project)) == 0
+        log0 = unify.log(project=project, **data)
+        log1 = unify.log(project=project, **data)
         assert log0 == log1
-        assert len(unify.get_logs_by_value(project, **data)) == 1
+        assert len(unify.get_logs_by_value(project=project, **data)) == 1
         log0.delete()
-        assert len(unify.get_logs_by_value(project, **data)) == 0
+        assert len(unify.get_logs_by_value(project=project, **data)) == 0
 
     def test_duplicate_log_field(self):
         project = "my_project"
@@ -75,9 +78,9 @@ class TestLogging(unittest.TestCase):
             "system_prompt": "You are a weather assistant",
             "user_prompt": "hello world",
         }
-        assert len(unify.get_logs(project)) == 0
-        log = unify.log(project, **data)
-        assert len(unify.get_logs(project)) == 1
+        assert len(unify.get_logs(project=project)) == 0
+        log = unify.log(project=project, **data)
+        assert len(unify.get_logs(project=project)) == 1
         new_data = {
             "system_prompt": "You are a maths assistant",
             "user_prompt": "hi earth",
@@ -94,8 +97,8 @@ class TestLogging(unittest.TestCase):
         if project in unify.list_projects():
             unify.delete_project(project)
         unify.create_project(project)
-        unify.log(project, my_func=my_func)
-        logs = unify.get_logs(project)
+        unify.log(project=project, my_func=my_func)
+        logs = unify.get_logs(project=project)
         assert len(logs) == 1
         assert (
             logs[0].entries["my_func"]
@@ -122,16 +125,16 @@ class TestLogging(unittest.TestCase):
             "user_prompt": "nothing",
             "score": 0.8,
         }
-        unify.log(project, **log1)
-        unify.log(project, **log2)
-        unify.log(project, **log3)
-        grouped_logs = unify.group_logs("system_prompt", project)
+        unify.log(project=project, **log1)
+        unify.log(project=project, **log2)
+        unify.log(project=project, **log3)
+        grouped_logs = unify.group_logs(key="system_prompt", project=project)
         assert len(grouped_logs) == 2
         assert sorted([version for version in grouped_logs]) == ["0", "1"]
 
         logs_metric = unify.get_logs_metric(
-            "mean",
-            "score",
+            metric="mean",
+            key="score",
             filter="'hello' in user_prompt",
             project="my_project",
         )
@@ -144,12 +147,12 @@ class TestLogging(unittest.TestCase):
         unify.create_project(project)
         for i in range(25):
             unify.log(
-                project,
+                project=project,
                 a=i,
                 b=i + 1,
                 c=i + 2,
             )
-        logs = unify.get_logs(project)
+        logs = unify.get_logs(project=project)
         for lg in logs:
             assert list(lg.entries.keys()) == ["a", "b", "c"]
 
@@ -158,7 +161,7 @@ class TestLogging(unittest.TestCase):
         if project in unify.list_projects():
             unify.delete_project(project)
         unify.create_project(project)
-        logs = unify.get_logs(project)
+        logs = unify.get_logs(project=project)
         assert len(logs) == 0, "There should be no logs initially."
         log_data1 = {
             "system_prompt": "You are a weather assistant",
@@ -179,9 +182,12 @@ class TestLogging(unittest.TestCase):
         }
         unify.log(project=project, **log_data3)
 
-        logs = unify.get_logs(project)
+        logs = unify.get_logs(project=project)
         assert len(logs) == 3, "There should be 3 logs in the project."
-        filtered_logs = unify.get_logs(project, filter="'weather' in user_prompt")
+        filtered_logs = unify.get_logs(
+            project=project,
+            filter="'weather' in user_prompt",
+        )
         assert (
             len(filtered_logs) == 1
         ), "There should be 1 log with 'weather' in the user prompt."
@@ -189,21 +195,21 @@ class TestLogging(unittest.TestCase):
             filtered_logs[0].entries.get("user_prompt") == log_data1["user_prompt"]
         ), "The filtered log should be the one that asks about the weather."
         nonexistent_logs = unify.get_logs(
-            project,
+            project=project,
             filter="'nonexistent' in user_prompt",
         )
         assert (
             len(nonexistent_logs) == 0
         ), "There should be no logs matching the nonexistent filter."
         multiple_filtered_logs = unify.get_logs(
-            project,
+            project=project,
             filter="'travel' in system_prompt and score < 0.5",
         )
         assert (
             len(multiple_filtered_logs) == 1
         ), "There should be 1 log with 'travel' in the user prompt and score > 0.5."
         bracket_logs = unify.get_logs(
-            project,
+            project=project,
             filter="('weather' in user_prompt) and ('weather' in system_prompt)",
         )
         assert (
@@ -212,20 +218,26 @@ class TestLogging(unittest.TestCase):
         assert (
             bracket_logs[0].entries.get("user_prompt") == log_data1["user_prompt"]
         ), "The filtered log should be the one that asks about the weather."
-        comparison_logs = unify.get_logs(project, filter="score > 0.5")
+        comparison_logs = unify.get_logs(project=project, filter="score > 0.5")
         assert len(comparison_logs) == 2, "There should be 2 logs with score > 0.5."
-        comparison_logs = unify.get_logs(project, filter="score == 0.9")
+        comparison_logs = unify.get_logs(project=project, filter="score == 0.9")
         assert len(comparison_logs) == 1, "There should be 1 log with score == 0.9."
-        logical_logs = unify.get_logs(project, filter="score > 0.5 and score < 0.8")
+        logical_logs = unify.get_logs(
+            project=project,
+            filter="score > 0.5 and score < 0.8",
+        )
         assert (
             len(logical_logs) == 1
         ), "There should be 1 log with score > 0.5 and score < 0.8."
-        logical_logs = unify.get_logs(project, filter="score < 0.5 or score > 0.8")
+        logical_logs = unify.get_logs(
+            project=project,
+            filter="score < 0.5 or score > 0.8",
+        )
         assert (
             len(logical_logs) == 2
         ), "There should be 2 logs with score < 0.5 or score > 0.8."
         string_comparison_logs = unify.get_logs(
-            project,
+            project=project,
             filter="user_prompt == 'What is the weather today?'",
         )
         assert (
@@ -238,19 +250,22 @@ class TestLogging(unittest.TestCase):
         if project in unify.list_projects():
             unify.delete_project(project)
         unify.create_project(project)
-        assert len(unify.get_logs(project)) == 0
-        unify.log(project, customer="John Smith")
-        unify.log(project, customer="Maggie Smith")
-        unify.log(project, customer="John Terry")
-        assert len(unify.get_logs(project)) == 3
-        deleted_logs = unify.delete_logs(project, "'Smith' in customer")
+        assert len(unify.get_logs(project=project)) == 0
+        unify.log(project=project, customer="John Smith")
+        unify.log(project=project, customer="Maggie Smith")
+        unify.log(project=project, customer="John Terry")
+        assert len(unify.get_logs(project=project)) == 3
+        deleted_logs = unify.delete_logs_by_value(
+            project=project,
+            filter="'Smith' in customer",
+        )
         assert len(deleted_logs) == 2
         assert set([dl.entries["customer"] for dl in deleted_logs]) == {
             "John Smith",
             "Maggie Smith",
         }
-        assert len(unify.get_logs(project)) == 1
-        deleted_logs = unify.delete_logs(project)
+        assert len(unify.get_logs(project=project)) == 1
+        deleted_logs = unify.delete_logs_by_value(project=project)
         assert len(deleted_logs) == 1
         assert deleted_logs[0].entries["customer"] == "John Terry"
-        assert len(unify.get_logs(project)) == 0
+        assert len(unify.get_logs(project=project)) == 0
