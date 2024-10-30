511a73387c62c99b5ee3be082d89a52ee9cf9de0
Daniel Lenton <daniel.lenton11@imperial.ac.uk>
2024-10-30 22:27:22 +0000

    added test_with_log_async.

--- tests/test_evals/test_logging.py
+++ tests/test_evals/test_logging.py
@@ -327,7 +327,32 @@ def test_with_log_threaded():
     ] + [[("d", i * 7 + 3), ("e", i * 7 + 4), ("f", i * 7 + 5)] for i in range(4)]
 
 
-# ToDo: implement test_with_log_async
+@pytest.mark.asyncio
+async def test_with_log_async():
+    project = "my_project"
+    if project in unify.list_projects():
+        unify.delete_project(project)
+    unify.create_project(project)
+    unify.activate(project)
+
+    async def fn(a, b, c, d, e, f, g):
+        with unify.Log(a=a):
+            unify.add_log_entries(b=b, c=c)
+            with unify.Log(d=d):
+                unify.add_log_entries(e=e, f=f)
+            unify.add_log_entries(g=g)
+
+    fns = [fn(*[7 * i + j for j in range(7)]) for i in range(4)]
+    await asyncio.gather(*fns)
+
+    logs = unify.get_logs(project="my_project")
+    entries = [log.entries for log in logs]
+
+    assert sorted([sorted(d.items()) for d in entries]) == [
+        [("a", i * 7), ("b", i * 7 + 1), ("c", i * 7 + 2), ("g", i * 7 + 6)]
+        for i in range(4)
+    ] + [[("d", i * 7 + 3), ("e", i * 7 + 4), ("f", i * 7 + 5)] for i in range(4)]
+
 
 # Entries
 
