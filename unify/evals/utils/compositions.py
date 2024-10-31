26c98baf45aadc27315d103557c81a44a1c63b19
Daniel Lenton <daniel.lenton11@imperial.ac.uk>
2024-10-31 12:16:23 +0000

    updated compositional log functions to include functions for either parameters or entries.

--- unify/evals/utils/compositions.py
+++ unify/evals/utils/compositions.py
@@ -2,7 +2,172 @@ from __future__ import annotations
 import json
 
 from ...utils.helpers import _validate_api_key, _get_and_maybe_create_project
+from .logging import _add_to_log
 from .logging import *
+
+
+# Helpers #
+# --------#
+
+
+def _replace_log_fields(
+    *,
+    logs: Optional[Union[int, unify.Log, List[Union[int, unify.Log]]]] = None,
+    mode: str = None,
+    api_key: Optional[str] = None,
+    **kwargs,
+) -> Dict[str, str]:
+    assert mode in (
+        "parameters",
+        "entries",
+    ), "mode must be one of 'parameters', 'entries'"
+    log_id = logs  # handle_multiple_logs decorator handles logs, returning a single id
+    api_key = _validate_api_key(api_key)
+    for k, v in kwargs.items():
+        delete_log_fields(field=k, logs=log_id, api_key=api_key)
+    return _add_to_log(logs=log_id, mode=mode, api_key=api_key, **kwargs)
+
+
+def _update_log_fields(
+    *,
+    fn: Union[callable, Dict[str, callable]],
+    logs: Optional[Union[int, unify.Log, List[Union[int, unify.Log]]]] = None,
+    mode: str = None,
+    api_key: Optional[str] = None,
+    **kwargs,
+) -> Dict[str, str]:
+    assert mode in (
+        "parameters",
+        "entries",
+    ), "mode must be one of 'parameters', 'entries'"
+    log_id = logs  # handle_multiple_logs decorator handles logs, returning a single id
+    data = getattr(get_log_by_id(id=log_id, api_key=api_key), mode)
+    replacements = dict()
+    for k, v in kwargs.items():
+        f = fn[k] if isinstance(fn, dict) else fn
+        replacements[k] = f(data[k], v)
+    return _replace_log_fields(logs=log_id, mode=mode, api_key=api_key, **replacements)
+
+
+def _rename_log_fields(
+    *,
+    logs: Optional[Union[int, unify.Log, List[Union[int, unify.Log]]]] = None,
+    mode: str = None,
+    api_key: Optional[str] = None,
+    **kwargs,
+) -> Dict[str, str]:
+    assert mode in (
+        "parameters",
+        "entries",
+    ), "mode must be one of 'parameters', 'entries'"
+    log_id = logs  # handle_multiple_logs decorator handles logs, returning a single id
+    api_key = _validate_api_key(api_key)
+    data = getattr(get_log_by_id(id=log_id, api_key=api_key), mode)
+    for old_name in kwargs.keys():
+        delete_log_fields(field=old_name, logs=log_id, api_key=api_key)
+    new_data = {new_name: data[old_name] for old_name, new_name in kwargs.items()}
+    return _add_to_log(logs=log_id, mode=mode, api_key=api_key, **new_data)
+
+
+# Parameters #
+# -----------#
+
+
+def replace_log_parameters(
+    *,
+    logs: Optional[Union[int, unify.Log, List[Union[int, unify.Log]]]] = None,
+    api_key: Optional[str] = None,
+    **kwargs,
+) -> Dict[str, str]:
+    """
+    Replaces existing parameters in an existing log.
+
+    Args:
+        logs: The log(s) to replace fields for. Looks for the current active log if none
+        specified.
+
+        api_key: If specified, unify API key to be used. Defaults to the value in the
+        `UNIFY_KEY` environment variable.
+
+        kwargs: The data to update in the log.
+
+    Returns:
+        A message indicating whether the log was successfully updated.
+    """
+    return _replace_log_fields(logs=logs, mode="parameters", api_key=api_key, **kwargs)
+
+
+def update_log_parameters(
+    *,
+    fn: Union[callable, Dict[str, callable]],
+    logs: Optional[Union[int, unify.Log, List[Union[int, unify.Log]]]] = None,
+    api_key: Optional[str] = None,
+    **kwargs,
+) -> Dict[str, str]:
+    """
+    Updates existing parameters in an existing log.
+
+    Args:
+        fn: The function or set of functions to apply to each field in the log.
+
+        logs: The log(s) to update fields for. Looks for the current active log if not
+        provided.
+
+        api_key: If specified, unify API key to be used. Defaults to the value in the
+        `UNIFY_KEY` environment variable.
+
+        kwargs: The data to update in the log.
+
+    Returns:
+        A message indicating whether the log was successfully updated.
+    """
+    return _update_log_fields(
+        fn=fn,
+        logs=logs,
+        mode="parameters",
+        api_key=api_key,
+        **kwargs,
+    )
+
+
+def rename_log_parameters(
+    *,
+    logs: Optional[Union[int, unify.Log, List[Union[int, unify.Log]]]] = None,
+    api_key: Optional[str] = None,
+    **kwargs,
+) -> Dict[str, str]:
+    """
+    Renames the set of log parameters.
+
+    Args:
+        logs: The log(s) to update the field names for. Looks for the current active log
+        if none are provided.
+
+        api_key: If specified, unify API key to be used. Defaults to the value in the
+        `UNIFY_KEY` environment variable.
+
+        kwargs: The field names to update in the log, with keys as old names and values
+        as new names.
+
+    Returns:
+        A message indicating whether the log field names were successfully updated.
+    """
+    return _rename_log_fields(logs=logs, mode="parameters", api_key=api_key, **kwargs)
+
+
+def group_logs_by_parameters(
+    *,
+    logs: List[unify.Log],
+) -> Dict:
+    configs = list(dict.fromkeys([json.dumps(lg.parameters) for lg in logs]))
+    ret_dict = dict()
+    for conf in configs:
+        ret_dict[conf] = [lg for lg in logs if json.dumps(lg.parameters) == conf]
+    return ret_dict
+
+
+# Entries #
+# --------#
 
 
 def replace_log_entries(
@@ -26,11 +191,7 @@ def replace_log_entries(
     Returns:
         A message indicating whether the log was successfully updated.
     """
-    log_id = logs  # handle_multiple_logs decorator handles logs, returning a single id
-    api_key = _validate_api_key(api_key)
-    for k, v in kwargs.items():
-        delete_log_fields(field=k, logs=log_id, api_key=api_key)
-    return add_log_entries(logs=log_id, api_key=api_key, **kwargs)
+    return _replace_log_fields(logs=logs, mode="entries", api_key=api_key, **kwargs)
 
 
 def update_log_entries(
@@ -57,13 +218,13 @@ def update_log_entries(
     Returns:
         A message indicating whether the log was successfully updated.
     """
-    log_id = logs  # handle_multiple_logs decorator handles logs, returning a single id
-    data = get_log_by_id(id=log_id, api_key=api_key).entries
-    replacements = dict()
-    for k, v in kwargs.items():
-        f = fn[k] if isinstance(fn, dict) else fn
-        replacements[k] = f(data[k], v)
-    return replace_log_entries(logs=log_id, api_key=api_key, **replacements)
+    return _update_log_fields(
+        fn=fn,
+        logs=logs,
+        mode="entries",
+        api_key=api_key,
+        **kwargs,
+    )
 
 
 def rename_log_entries(
@@ -88,13 +249,11 @@ def rename_log_entries(
     Returns:
         A message indicating whether the log field names were successfully updated.
     """
-    log_id = logs  # handle_multiple_logs decorator handles logs, returning a single id
-    api_key = _validate_api_key(api_key)
-    data = get_log_by_id(id=log_id, api_key=api_key).entries
-    for old_name in kwargs.keys():
-        delete_log_fields(field=old_name, logs=log_id, api_key=api_key)
-    new_entries = {new_name: data[old_name] for old_name, new_name in kwargs.items()}
-    return add_log_entries(logs=log_id, api_key=api_key, **new_entries)
+    return _rename_log_fields(logs=logs, mode="entries", api_key=api_key, **kwargs)
+
+
+# Fields (Both) #
+# --------------#
 
 
 # noinspection PyShadowingBuiltins
@@ -281,14 +440,3 @@ def group_logs(
         )
         for k, v in get_groups(key=key, project=project, api_key=api_key).items()
     }
-
-
-def group_logs_by_params(
-    *,
-    logs: List[unify.Log],
-) -> Dict:
-    configs = list(dict.fromkeys([json.dumps(lg.parameters) for lg in logs]))
-    ret_dict = dict()
-    for conf in configs:
-        ret_dict[conf] = [lg for lg in logs if json.dumps(lg.parameters) == conf]
-    return ret_dict
