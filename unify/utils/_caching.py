ad7fc683e130aae357eaa7a2b95bfb2a84b6b768
Daniel Lenton <daniel.lenton11@imperial.ac.uk>
2024-11-06 23:17:57 +0000

    improved _write_to_cache, with better support for nested types.

--- unify/utils/_caching.py
+++ unify/utils/_caching.py
@@ -1,6 +1,7 @@
 import json
 import os
 import threading
+from pydantic import BaseModel
 from typing import Dict, Optional, Union, Any
 from openai.types.chat import ChatCompletion
 
@@ -37,14 +38,29 @@ def _get_cache(kw: Dict[str, Any]) -> Union[None, Dict]:
     CACHE_LOCK.release()
 
 
+def _dumps(obj: Any) -> Any:
+    if isinstance(obj, BaseModel):
+        return obj.model_dump()
+    elif hasattr(obj, "to_json"):
+        return obj.to_json()
+    elif isinstance(obj, dict):
+        return json.dumps({k: _dumps(v) for k, v in obj.items()})
+    elif isinstance(obj, list):
+        return json.dumps([_dumps(v) for v in obj])
+    elif isinstance(obj, tuple):
+        return json.dumps(tuple(_dumps(v) for v in obj))
+    else:
+        return obj
+
+
 # noinspection PyTypeChecker,PyUnresolvedReferences
 def _write_to_cache(kw, response):
     global CACHE_LOCK
     CACHE_LOCK.acquire()
     _create_cache_if_none()
     kw = {k: v for k, v in kw.items() if v is not None}
-    kw_str = json.dumps(kw)
-    response_str = json.dumps(response.model_dump())
+    kw_str = _dumps(kw)
+    response_str = _dumps(response)
     _cache[kw_str] = response_str
     with open(_cache_fpath, "w") as outfile:
         json.dump(_cache, outfile)
