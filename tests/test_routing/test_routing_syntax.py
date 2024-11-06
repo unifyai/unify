b64fd54892b5960a898bcb92aff172fd58d49a0f
Daniel Lenton <daniel.lenton11@imperial.ac.uk>
2024-11-06 14:44:20 +0000

    added tests for all meta-provider metrics, and updated helper accordingly.

--- tests/test_routing/test_routing_syntax.py
+++ tests/test_routing/test_routing_syntax.py
@@ -1,7 +1,38 @@
 import unify
 
 
+def test_time_to_first_token():
+    for pre in ("", "lowest-"):
+        unify.Unify(f"claude-3-opus@{pre}time-to-first-token").generate("Hello.")
+        unify.Unify(f"gpt-4o@{pre}ttft").generate("Hello.")
+        unify.Unify(f"mixtral-8x22b-instruct-v0.1@{pre}t").generate("Hello.")
+
+
 def test_inter_token_latency():
-    unify.Unify("claude-3-opus@inter-token-latency").generate("Hello.")
-    unify.Unify("gpt-4o@itl").generate("Hello.")
-    unify.Unify("mixtral-8x22b-instruct-v0.1@i").generate("Hello.")
+    for pre in ("", "lowest-"):
+        unify.Unify(f"claude-3-opus@{pre}inter-token-latency").generate("Hello.")
+        unify.Unify(f"gpt-4o@{pre}itl").generate("Hello.")
+        unify.Unify(f"mixtral-8x22b-instruct-v0.1@{pre}i").generate("Hello.")
+
+
+def test_cost():
+    for pre in ("", "lowest-"):
+        unify.Unify(f"claude-3-opus@{pre}cost").generate("Hello.")
+        unify.Unify(f"mixtral-8x22b-instruct-v0.1@{pre}c").generate("Hello.")
+
+
+def test_input_cost():
+    for pre in ("", "lowest-"):
+        unify.Unify(f"claude-3-opus@{pre}input-cost").generate("Hello.")
+        unify.Unify(f"gpt-4o@{pre}ic").generate("Hello.")
+        unify.Unify(f"mixtral-8x22b-instruct-v0.1@{pre}i").generate("Hello.")
+
+
+def test_output_cost():
+    for pre in ("", "lowest-"):
+        unify.Unify(f"claude-3-opus@{pre}output-cost").generate("Hello.")
+        unify.Unify(f"mixtral-8x22b-instruct-v0.1@{pre}oc").generate("Hello.")
+
+
+if __name__ == "__main__":
+    pass
