02e6200ea575eb27beb05c644c72fc8dd7d3cb8b
Daniel Lenton <daniel.lenton11@imperial.ac.uk>
2025-01-08 00:32:50 +0000

    small fixes to project testing, to make the test isolated rather than read and write to the same project.

--- tests/test_evals/test_projects.py
+++ tests/test_evals/test_projects.py
@@ -31,20 +31,22 @@ def test_with_project():
 def test_set_project_then_log():
     unify.deactivate()
     assert unify.active_project is None
-    unify.activate("my_project")
-    assert unify.active_project == "my_project"
+    unify.activate("test_set_project_then_log")
+    assert unify.active_project == "test_set_project_then_log"
     unify.log(key=1.0)
     unify.deactivate()
     assert unify.active_project is None
+    unify.delete_project("test_set_project_then_log")
 
 
 def test_with_project_then_log():
     unify.deactivate()
     assert unify.active_project is None
-    with unify.Project("my_project"):
-        assert unify.active_project == "my_project"
+    with unify.Project("test_with_project_then_log"):
+        assert unify.active_project == "test_with_project_then_log"
         unify.log(key=1.0)
     assert unify.active_project is None
+    unify.delete_project("test_with_project_then_log")
 
 
 if __name__ == "__main__":
