e285381734eea7896c2838a8693929076cd93299
Daniel <daniel.lenton11@imperial.ac.uk>
2025-02-02 02:29:09 +0200

    added + tested read-only mode for llm caching.

--- tests/test_utils/test_caching.py
+++ tests/test_utils/test_caching.py
@@ -7,7 +7,7 @@ from unify.utils._caching import _cache_fpath
 
 
 # noinspection PyBroadException
-def test_basic_caching() -> None:
+def test_cache() -> None:
     local_cache_path = _cache_fpath.replace(".cache.json", ".test_cache.json")
     try:
         unify.utils._caching._cache_fpath = local_cache_path
@@ -38,7 +38,7 @@ def test_basic_caching() -> None:
 
 
 # noinspection PyBroadException
-def test_cache_writing() -> None:
+def test_cache_write() -> None:
     local_cache_path = _cache_fpath.replace(".cache.json", ".test_cache.json")
     try:
         unify.utils._caching._cache_fpath = local_cache_path
@@ -63,7 +63,7 @@ def test_cache_writing() -> None:
 
 
 # noinspection PyBroadException
-def test_cache_reading() -> None:
+def test_cache_read() -> None:
     local_cache_path = _cache_fpath.replace(".cache.json", ".test_cache.json")
     try:
         unify.utils._caching._cache_fpath = local_cache_path
@@ -85,6 +85,44 @@ def test_cache_reading() -> None:
         assert mt0 == mt1
         assert r0 == r1
         os.remove(local_cache_path)
+        global_cache_path = local_cache_path.replace(".test_cache.json", ".cache.json")
+        unify.utils._caching._cache_fpath = global_cache_path
+    except Exception as e:
+        if os.path.exists(local_cache_path):
+            os.remove(local_cache_path)
+        raise e
+
+
+# noinspection PyBroadException
+def test_cache_read_only() -> None:
+    local_cache_path = _cache_fpath.replace(".cache.json", ".test_cache.json")
+    try:
+        unify.utils._caching._cache_fpath = local_cache_path
+        if os.path.exists(local_cache_path):
+            os.remove(local_cache_path)
+        client = Unify(
+            endpoint="gpt-4o@openai",
+        )
+        t = time.perf_counter()
+        r0 = client.generate(user_message="hello", cache="write")
+        t0 = time.perf_counter() - t
+        assert os.path.exists(local_cache_path)
+        mt0 = os.path.getmtime(local_cache_path)
+        t = time.perf_counter()
+        r1 = client.generate(user_message="hello", cache="read-only")
+        mt1 = os.path.getmtime(local_cache_path)
+        t1 = time.perf_counter() - t
+        assert t1 < t0
+        assert mt0 == mt1
+        assert r0 == r1
+        os.remove(local_cache_path)
+        unify._caching._cache = None
+        try:
+            client.generate(user_message="hello", cache="read-only")
+            raised_exception = False
+        except Exception:
+            raised_exception = True
+        assert raised_exception, "read-only mode should have raised exception"
         global_cache_path = local_cache_path.replace(".test_cache.json", ".cache.json")
         unify.utils._caching._cache_fpath = global_cache_path
     except Exception as e:
