23437f43c2ebe49316aeef3c2057bdace11a3436
djl11 <dan@unify.ai>
2025-02-24 21:07:28 +0700

    removed depracted router tests.

--- tests/test_routing/test_routing_syntax.py
+++ tests/test_routing/test_routing_syntax.py
@@ -4,12 +4,6 @@ import unify
 # ---------------#
 
 
-def test_quality():
-    for pre in ("", "highest-"):
-        unify.Unify(f"claude-3-opus@{pre}quality").generate("Hello.")
-        unify.Unify(f"mixtral-8x22b-instruct-v0.1@{pre}q").generate("Hello.")
-
-
 def test_ttft():
     for pre in ("", "lowest-"):
         unify.Unify(f"claude-3-opus@{pre}time-to-first-token").generate("Hello.")
@@ -50,23 +44,8 @@ def test_output_cost():
 def test_thresholds():
     unify.Unify("llama-3.1-405b-chat@inter-token-latency|c<5").generate("Hello.")
     unify.Unify(
-        "llama-3.1-70b-chat@quality|input-cost<=0.8|output-cost<=0.8|itl>1|itl<20",
+        "llama-3.1-70b-chat@ttft|input-cost<=0.8|output-cost<=0.8|itl>1|itl<20",
     ).generate("Hello.")
-
-
-# Routing #
-# --------#
-
-
-def test_routing():
-    unify.Unify(
-        "router@quality|input-cost<0.8|output-cost<0.6|itl<20",
-    ).generate("Hello.")
-
-
-def test_routing_w_custom_metric():
-    unify.Unify("router@q:1|i:0.5|t:2|c:0.7").generate("Hello.")
-    unify.Unify("router@q:1|i:0.5").generate("Hello.")
 
 
 # Search Space #
@@ -82,31 +61,6 @@ def test_routing_w_providers():
 def test_routing_skip_providers():
     unify.Unify(
         "llama-3.1-405b-chat@itl|skip_providers:vertex-ai,aws-bedrock",
-    ).generate("Hello.")
-
-
-def test_routing_w_models():
-    unify.Unify(
-        "router@q:1|i:0.5|models:gpt-4o,o3-mini,claude-3-sonnet",
-    ).generate("Hello.")
-
-
-def test_routing_skip_models():
-    unify.Unify(
-        "router@q:1|i:0.5|skip_models:gpt-4o,o3-mini,claude-3-sonnet",
-    ).generate("Hello.")
-
-
-def test_routing_w_models_skip_providers():
-    unify.Unify(
-        "router@q:1|i:0.5|models:claude-3-haiku,claude-3-sonnet|"
-        "skip_providers:aws-bedrock",
-    ).generate("Hello.")
-
-
-def test_routing_w_providers_skip_models():
-    unify.Unify(
-        "router@q:1|i:0.5|providers:aws-bedrock|skip_models:claude-3-haiku",
     ).generate("Hello.")
 
 
