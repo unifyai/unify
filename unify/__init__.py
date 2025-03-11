750ff9dd77b89314af4d396932b3a0e06e894b1a
CatB1t <skytedits@gmail.com>
2025-03-11 07:34:55 +0200

    Remove redundant function call in create_project.

--- unify/__init__.py
+++ unify/__init__.py
@@ -91,8 +91,7 @@ def activate(project: str, overwrite: bool = False, api_key: str = None) -> None
     if project not in list_projects(api_key=api_key):
         create_project(project, api_key=api_key)
     elif overwrite:
-        delete_project(project, api_key=api_key)
-        create_project(project, api_key=api_key)
+        create_project(project, api_key=api_key, overwrite=True)
     global PROJECT
     PROJECT = project
 
