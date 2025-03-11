490234b60acb452096f28fbe9e2ff0273b9fe60a
CatB1t <skytedits@gmail.com>
2025-03-11 06:01:27 +0200

    Update request debug msg, add env varialbe, add missing debug log functions.

--- unify/utils/_requests.py
+++ unify/utils/_requests.py
@@ -1,21 +1,35 @@
+import json
 import logging
+import os
 
 import requests
 
-_handler = logging.FileHandler("requests.log", mode="w")
-_handler.setLevel(logging.DEBUG)
-_handler.formatter = logging.Formatter("%(levelname)s:%(name)s:%(asctime)s:%(message)s")
-
 _logger = logging.getLogger("unify_requests")
-_logger.setLevel(logging.DEBUG)
-_logger.addHandler(_handler)
+_log_enabled = os.getenv("UNIFY_REQUESTS_DEBUG", "false").lower() in ("true", "1")
+if _log_enabled:
+    _handler = logging.FileHandler("requests.log", mode="w")
+    _handler.setLevel(logging.DEBUG)
+    _handler.formatter = logging.Formatter(
+        "%(levelname)s:%(name)s:%(asctime)s:%(message)s",
+    )
+
+    _logger.setLevel(logging.DEBUG)
+    _logger.addHandler(_handler)
 
 
 def _log(type, url: str, /, **kwargs):
+    if not _log_enabled:
+        return
+    _kwargs_str = ""
+    for k, v in kwargs.items():
+        if isinstance(v, dict):
+            _kwargs_str += f"{k:}:{json.dumps(v, indent=2)},\n"
+        else:
+            _kwargs_str += f"{k}:{v},\n"
     log_msg = f"""
-    ====== {type} =======
-    url:{url}
-    msg:{kwargs}
+====== {type} =======
+url:{url}
+{_kwargs_str}
 """
     _logger.debug(log_msg)
 
@@ -26,32 +40,35 @@ def request(method, url, **kwargs):
 
 
 def get(url, params=None, **kwargs):
-    _log("get", url, params=params, **kwargs)
+    _log("GET", url, params=params, **kwargs)
     return requests.get(url, params=params, **kwargs)
 
 
 def options(url, **kwargs):
+    _log("OPTIONS", url, **kwargs)
     return requests.options(url, **kwargs)
 
 
 def head(url, **kwargs):
+    _log("HEAD", url, **kwargs)
     return requests.head(url, **kwargs)
 
 
 def post(url, data=None, json=None, **kwargs):
-    _log("post", url, data=data, json=json, **kwargs)
+    _log("POST", url, data=data, json=json, **kwargs)
     return requests.post(url, data=data, json=json, **kwargs)
 
 
 def put(url, data=None, **kwargs):
-    _log("put", url, data=data, **kwargs)
+    _log("PUT", url, data=data, **kwargs)
     return requests.put(url, data=data, **kwargs)
 
 
 def patch(url, data=None, **kwargs):
+    _log("PATCH", url, data=data, **kwargs)
     return requests.patch(url, data=data, **kwargs)
 
 
 def delete(url, **kwargs):
-    _log("delete", url, **kwargs)
+    _log("DELETE", url, **kwargs)
     return requests.delete(url, **kwargs)
