9496e64bc225983ae56adcee9c1bab40d2d40bd5
Daniel Lenton <daniel.lenton11@imperial.ac.uk>
2025-03-13 19:46:12 +0000

    moved _check_response to utils.helpers, where all other helper functions are stored.

--- unify/utils/helpers.py
+++ unify/utils/helpers.py
@@ -12,6 +12,22 @@ from pydantic import BaseModel, ValidationError
 PROJECT_LOCK = threading.Lock()
 
 
+class RequestError(Exception):
+    def __init__(self, response: requests.Response):
+        req = response.request
+        message = (
+            f"{req.method} {req.url} failed with status code {response.status_code}. "
+            f"Request body: {req.body}, Response: {response.text}"
+        )
+        super().__init__(message)
+        self.response = response
+
+
+def _check_response(response: requests.Response):
+    if not response.ok:
+        raise RequestError(response)
+
+
 def _res_to_list(response: requests.Response) -> Union[List, Dict]:
     return json.loads(response.text)
 
