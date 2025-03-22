3b025755d0ff0b74ce71e89660ba8472100be7db
djl11 <dan@unify.ai>
2025-03-22 18:48:46 +0000

    updated user-input mode to support full ChatCompletion returns.

--- tests/test_universal_api/test_user_input.py
+++ tests/test_universal_api/test_user_input.py
@@ -2,23 +2,19 @@ import builtins
 import traceback
 
 import unify
+from openai.types.chat import ChatCompletion
 
 
 class SimulateInput:
     def __init__(self):
-        self._messages = [
-            "Hi, how can I help you?",
-        ]
-        self._count = 0
+        self._message = "Hi, how can I help you?"
         self._true_input = None
 
     def _new_input(self, user_instructions):
         if user_instructions is not None:
             print(user_instructions)
-        message = self._messages[self._count]
-        self._count += 1
-        print(message)
-        return message
+        print(self._message)
+        return self._message
 
     def __enter__(self):
         self._true_input = builtins.__dict__["input"]
@@ -36,7 +32,10 @@ class SimulateInput:
 def test_user_input_client():
     client = unify.Unify("user-input")
     with SimulateInput():
-        client.generate("hello")
+        response = client.generate("hello")
+        assert isinstance(response, str)
+        response = client.generate("hello", return_full_completion=True)
+        assert isinstance(response, ChatCompletion)
 
 
 if __name__ == "__main__":
