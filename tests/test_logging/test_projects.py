5d9d80d188f349c18eb1f7b3894c809f523960a8
djl11 <dan@unify.ai>
2025-04-07 16:41:08 +0100

    added optional UNIFY_PROJECT environment variable.

--- tests/test_logging/test_projects.py
+++ tests/test_logging/test_projects.py
@@ -1,3 +1,5 @@
+import os
+
 import unify
 
 
@@ -45,6 +47,20 @@ def test_with_project_then_log():
         unify.log(key=1.0)
     assert unify.active_project() is None
     unify.delete_project("test_with_project_then_log")
+
+
+def test_project_env_var():
+    unify.deactivate()
+    assert unify.active_project() is None
+    os.environ["UNIFY_PROJECT"] = "test_project_env_var"
+    assert unify.active_project() == "test_project_env_var"
+    unify.log(x=0, y=1, z=2)
+    del os.environ["UNIFY_PROJECT"]
+    assert unify.active_project() is None
+    logs = unify.get_logs(project="test_project_env_var")
+    assert len(logs) == 1
+    assert logs[0].entries == {"x": 0, "y": 1, "z": 2}
+    unify.delete_project("test_project_env_var")
 
 
 if __name__ == "__main__":
