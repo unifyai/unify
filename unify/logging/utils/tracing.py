46735a449ef3b3e52b8f5e7532463dfc2fe91aa3
CatB1t <skytedits@gmail.com>
2025-05-13 10:40:42 +0300

    Add tracing import hook.

---
+++ unify/logging/utils/tracing.py
@@ -0,0 +1,50 @@
+import importlib
+import sys
+
+from unify import traced
+
+
+class TraceLoader(importlib.abc.Loader):
+    def __init__(self, original_loader, filter=None):
+        self._original_loader = original_loader
+        self.filter = filter
+
+    def create_module(self, spec):
+        return self._original_loader.create_module(spec)
+
+    def exec_module(self, module):
+        self._original_loader.exec_module(module)
+        traced(module, filter=self.filter)
+
+
+class TraceFinder(importlib.abc.MetaPathFinder):
+    def __init__(self, targets=[], filter=None):
+        self.targets = targets
+        self.filter = filter
+        self._original_finder = importlib.machinery.PathFinder
+
+    def find_spec(self, fullname, path, target=None):
+        for target_module in self.targets:
+            if not fullname.startswith(target_module):
+                return None
+
+        spec = self._original_finder.find_spec(fullname, path)
+        if not spec or not spec.loader:
+            return None
+
+        if spec.origin is None or not spec.origin.endswith(".py"):
+            return None
+
+        spec.loader = TraceLoader(spec.loader, filter=self.filter)
+        return spec
+
+
+def install_tracing_hook(targets=[], filter=None):
+    if not any(isinstance(finder, TraceFinder) for finder in sys.meta_path):
+        sys.meta_path.insert(0, TraceFinder(targets, filter))
+
+
+def disable_tracing_hook():
+    for finder in sys.meta_path:
+        if isinstance(finder, TraceFinder):
+            sys.meta_path.remove(finder)
