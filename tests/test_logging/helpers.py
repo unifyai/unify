1481a4b5ca7ac6ba9173a4003982881f1459adaf
CatB1t <skytedits@gmail.com>
2025-05-20 14:09:22 +0300

    Fix _handle_project helper in testing to raise exceptions for async tests.

--- tests/test_logging/helpers.py
+++ tests/test_logging/helpers.py
@@ -1,3 +1,4 @@
+import asyncio
 import functools
 import sys
 import traceback
@@ -22,4 +23,19 @@ def _handle_project(test_fn):
             tb_string = "".join(traceback.format_exception(exc_type, exc_value, exc_tb))
             raise Exception(f"{tb_string}")
 
-    return wrapper
+    @functools.wraps(test_fn)
+    async def async_wrapper(*args, **kwargs):
+        project = test_fn.__name__
+        if project in unify.list_projects():
+            unify.delete_project(project)
+        try:
+            with unify.Project(project):
+                await test_fn(*args, **kwargs)
+            unify.delete_project(project)
+        except:
+            unify.delete_project(project)
+            exc_type, exc_value, exc_tb = sys.exc_info()
+            tb_string = "".join(traceback.format_exception(exc_type, exc_value, exc_tb))
+            raise Exception(f"{tb_string}")
+
+    return async_wrapper if asyncio.iscoroutinefunction(test_fn) else wrapper
