fcbb8e4f66ce934c8f96fcc794c5e44455a2b685
CatB1t <skytedits@gmail.com>
2025-05-28 12:59:05 +0300

    Fix with.Traced handling of Store and AugAssign, update test.

--- tests/test_logging/test_tracing.py
+++ tests/test_logging/test_tracing.py
@@ -743,12 +743,16 @@ def test_with_traced_context():
     ll = []
     msg = "Bye"
     e = 10
+    f = "a"
+    g = 0
     with unify.Traced("First Context"):
         c = a + b
         b = 20
         d = b - 20
         msg = f"Hello {msg}"
         ll.append(0)
+        f = "b"
+        g += 1
 
     _wait_for_trace_logger()
     logs = unify.get_logs()
@@ -756,8 +760,16 @@ def test_with_traced_context():
     trace = logs[0].entries["trace"]
     assert trace["span_name"] == "First Context"
     assert trace["type"] == "context"
-    assert trace["inputs"] == {"a": 0, "b": 1, "ll": [], "msg": "Bye"}
-    assert trace["outputs"] == {"c": 1, "b": 20, "d": 0, "msg": "Hello Bye", "ll": [0]}
+    assert trace["inputs"] == {"a": 0, "b": 1, "ll": [], "msg": "Bye", "f": "a", "g": 0}
+    assert trace["outputs"] == {
+        "c": 1,
+        "b": 20,
+        "d": 0,
+        "msg": "Hello Bye",
+        "ll": [0],
+        "f": "b",
+        "g": 1,
+    }
     assert len(trace["child_spans"]) == 0
 
 
