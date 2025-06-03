9b57b6cfecbb1205104d6482572032250408a015
djl11 <dan@unify.ai>
2025-06-03 17:11:56 +0100

    added include_class_name: bool to methods_to_tool_dict.

--- unity/common/llm_helpers.py
+++ unity/common/llm_helpers.py
@@ -41,11 +41,18 @@ _MANAGEMENT_METHOD_NAMES: set[str] = {
 }
 
 
-def methods_to_tool_dict(*methods: Tuple[Callable]) -> Dict[str, Callable]:
-    return {
-        f"{m.__self__.__class__.__name__}_{m.__name__}".replace("__", "_"): m
-        for m in methods
-    }
+def methods_to_tool_dict(
+    *methods: Tuple[Callable],
+    include_class_name: bool = True,
+) -> Dict[str, Callable]:
+    ret = dict()
+    for m in methods:
+        if include_class_name:
+            key = f"{m.__self__.__class__.__name__}_{m.__name__}".replace("__", "_")
+        else:
+            key = m.__name__.lstrip("_")
+        ret[key] = m
+    return ret
 
 
 def _discover_custom_public_methods(handle) -> dict[str, Callable]:
