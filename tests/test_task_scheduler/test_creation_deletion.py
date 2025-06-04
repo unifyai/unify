54f17b5a32820c09043c8f4b886c8d1b88196991
djl11 <dan@unify.ai>
2025-06-04 22:53:11 +0100

    small fix for test_create_task.

--- tests/test_task_scheduler/test_creation_deletion.py
+++ tests/test_task_scheduler/test_creation_deletion.py
@@ -18,8 +18,8 @@ def test_create_task():
         {
             "name": "Promote Jeff Smith",
             "description": "Send an email to Jeff Smith, kindly congratulating him and explaining that he has been promoted from sales rep to sales manager.",
-            "status": Status.active,
-            "schedule": {"prev_task": None, "next_task": None},
+            "status": Status.primed,
+            "schedule": None,
             "deadline": None,
             "repeat": None,
             "priority": Priority.normal,
