9e29b6ab9fba1da1324cc4ca4637290e7a9b0e9b
vedpatwardhan <vedpat3@gmail.com>
2025-07-18 12:21:52 +0530

    enabled the staging startup topic as well in the comms manager

--- unity/conversation_manager/comms_manager.py
+++ unity/conversation_manager/comms_manager.py
@@ -8,7 +8,10 @@ from unity.conversation_manager.events import *
 
 # Subscription IDs
 project_id = "gcp-project-runtime"
-startup_subscription_id = "unity-startup-sub"
+startup_subscription_id = (
+    "unity-startup-sub"
+    + ("-staging" if os.getenv("STAGING") else "")
+)
 subscription_id = (
     "unity-"
     + (os.getenv("ASSISTANT_ID") if os.getenv("ASSISTANT_ID") else "default-assistant")
