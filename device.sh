861c514a45beb5cb7f8abb713ff155a48a5a2b94
juliagsy <67888047+juliagsy@users.noreply.github.com>
2025-07-24 15:18:42 +0800

    added commented code for virtual cam setup (more setup needed before usage)

--- device.sh
+++ device.sh
@@ -6,3 +6,11 @@ sleep 2
 fluxbox &
 x11vnc -display :99 -nopw -forever -shared -bg -rfbport 5900
 websockify --web=/opt/novnc 6080 localhost:5900
+
+## Set up for virtual camera (host.sh)
+# sudo apt-get update && sudo apt-get install -y v4l2loopback-dkms v4l2loopback-utils v4l-utils linux-modules-extra-$(uname -r)
+
+# sudo modprobe v4l2loopback devices=1 video_nr=10 card_label="TestCam" exclusive_caps=1
+
+# sudo usermod -aG video $USER
+# newgrp video
