8edd6867eca0c5c2e50555059644e072f072b573
CatB1t <skytedits@gmail.com>
2025-08-13 12:05:27 +0300

    Add abstract BaseCache

--- unify/utils/caching/_base_cache.py
+++ unify/utils/caching/_base_cache.py
@@ -1,6 +1,55 @@
-from abc import ABC
+from abc import ABC, abstractmethod
+from typing import Any, Dict, List, Optional
 
 
 class BaseCache(ABC):
-    # TODO: Implement a base class for caching
-    pass
+    @classmethod
+    @abstractmethod
+    def set_filename(cls, filename: str) -> None:
+        pass
+
+    @classmethod
+    @abstractmethod
+    def get_filename(cls) -> str:
+        pass
+
+    @classmethod
+    @abstractmethod
+    def update_entry(
+        cls,
+        *,
+        key: str,
+        value: Any,
+        res_types: Optional[Dict[str, Any]] = None,
+    ) -> None:
+        pass
+
+    @classmethod
+    @abstractmethod
+    def write(cls, filename: str = None) -> None:
+        pass
+
+    @classmethod
+    @abstractmethod
+    def create_or_load(cls, filename: str = None) -> None:
+        pass
+
+    @classmethod
+    @abstractmethod
+    def get_keys(cls) -> List[str]:
+        pass
+
+    @classmethod
+    @abstractmethod
+    def get_entry(cls, cache_key: str) -> Optional[Any]:
+        pass
+
+    @classmethod
+    @abstractmethod
+    def key_exists(cls, cache_key: str) -> bool:
+        pass
+
+    @classmethod
+    @abstractmethod
+    def delete(cls, cache_key: str) -> None:
+        pass
