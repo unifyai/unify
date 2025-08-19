b9e8b7d4f13d5c8edd36cb40f79b8052e9ac8758
juliagsy <67888047+juliagsy@users.noreply.github.com>
2025-08-19 16:14:04 +0800

    more fixes for virtual mic

--- entrypoint.sh
+++ entrypoint.sh
@@ -58,6 +58,16 @@ echo "Redis started with PID: $REDIS_PID"
 
 
 # Virtual desktop and devices
+# Set up for virtual audio
+export XDG_RUNTIME_DIR=/tmp/runtime-root
+mkdir -p $XDG_RUNTIME_DIR
+chmod 700 $XDG_RUNTIME_DIR
+
+mkdir -p /run/dbus
+dbus-daemon --system --fork
+eval "$(dbus-launch)"
+export DBUS_SESSION_BUS_ADDRESS
+
 bash desktop.sh &
 DESKTOP_PID=$!
 
