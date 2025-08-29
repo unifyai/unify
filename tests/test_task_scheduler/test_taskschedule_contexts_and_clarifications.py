312195492247c4d0b22ba37fc4ca82f00ee9c0e5
djl11 <dan@unify.ai>
2025-08-29 09:55:59 +0100

    re-added timeouts to asyncio.wait_for commands, which require this argument for python 3.11 [skip ci]

--- tests/test_task_scheduler/test_taskschedule_contexts_and_clarifications.py
+++ tests/test_task_scheduler/test_taskschedule_contexts_and_clarifications.py
@@ -90,7 +90,7 @@ async def test_ts_ask_requests_clarification():
     )
 
     # expect a clarification question
-    question = await asyncio.wait_for(up_q.get())
+    question = await asyncio.wait_for(up_q.get(), timeout=300)
     assert _contains(question.lower(), "high") and _contains(
         question.lower(),
         "priority",
@@ -197,13 +197,13 @@ async def test_ts_update_requests_clarification():
     )
 
     # clarification expected
-    q_text = await asyncio.wait_for(up_q.get())
+    q_text = await asyncio.wait_for(up_q.get(), timeout=300)
     assert _contains(q_text, "queued"), "No clarification question"
 
     # clarify we mean the slide-deck task
     await down_q.put("I mean the Write quarterly report task.")
 
-    await asyncio.wait_for(task)
+    await asyncio.wait_for(task, timeout=300)
 
     row = ts._filter_tasks(filter=f"task_id == {tid_report}", limit=1)[0]
     assert row["priority"] == "high", assertion_failed(
