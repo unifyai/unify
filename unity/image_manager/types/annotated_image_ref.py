ac2e87eb78ed8c38f2d497c83655346e028219c4
djl11 <dan@unify.ai>
2025-10-14 18:01:51 -0700

    refactor(image_manager/types): simplify RawImageRef to hold image_id (int) instead of Image and update test_image_types to construct RawImageRef(image_id) and adjust assertions

--- unity/image_manager/types/annotated_image_ref.py
+++ unity/image_manager/types/annotated_image_ref.py
@@ -14,5 +14,5 @@ class AnnotatedImageRef(BaseModel):
         description="Reference to the underlying raw image",
     )
     annotation: str = Field(
-        description="Context-specific relevance annotation for the image",
+        description="Context-specific relevance annotation for the image, aligning it with some correspond text",
     )
