dfd4ae4be1d6e852e41b3924b097c050f9052f05
Infrared1029 <60873139+Infrared1029@users.noreply.github.com>
2025-10-24 00:14:39 +0300

    Realtime support v0 set up (#100)
    * v0 set up
    
    * bug fixes

--- unity/conversation_manager_2/state.py
+++ unity/conversation_manager_2/state.py
@@ -6,7 +6,7 @@ from typing import Literal, Optional
 from collections import deque
 
 from pydantic import Field
-from unity.conversation_manager_2.actions import build_dynamic_response_models
+# from unity.conversation_manager_2.actions import build_dynamic_response_models
 from unity.conversation_manager_2.new_events import *
 from unity.contact_manager.types.contact import Contact as ContactType
 from unity.transcript_manager.types.message import UNASSIGNED
@@ -74,7 +74,10 @@ class ConversationManagerState:
         user_number: str,
         user_email: str,
         user_whatsapp_number: str,
+        realtime: bool = False
     ):
+        
+        self.realtime = realtime
 
         # These should not be harcoded
         self.phone_contacts_map = {
@@ -550,6 +553,10 @@ class ConversationManagerState:
 
     def build_response_model(self):
         """Build dynamic response models based on available actions."""
+        if self.realtime:
+            from unity.conversation_manager_2.actions_realtime import build_dynamic_response_models
+        else:
+            from unity.conversation_manager_2.actions import build_dynamic_response_models
         self.dynamic_response_models = build_dynamic_response_models(
             include_email=self.assistant_email not in [None, ""],
             include_sms=self.assistant_number not in [None, ""],
