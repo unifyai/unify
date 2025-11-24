511d48cfe2a00ee7f5c4471348d1108dcf3fc24e
vedpatwardhan <vedpat3@gmail.com>
2025-11-24 13:14:01 +0530

    updated the call scripts to avoid agent dispatch when download-files

--- unity/conversation_manager/medium_scripts/call.py
+++ unity/conversation_manager/medium_scripts/call.py
@@ -346,9 +346,10 @@ if __name__ == "__main__":
     agent_name = f"unity_{assistant_number}"
 
     # dispatch agent
-    print(f"Dispatching agent {agent_name}")
-    dispatch_agent(agent_name)
-    print(f"Agent {agent_name} dispatched")
+    if sys.argv[1] != "download-files":
+        print(f"Dispatching agent {agent_name}")
+        dispatch_agent(agent_name)
+        print(f"Agent {agent_name} dispatched")
 
     agents.cli.run_app(
         agents.WorkerOptions(
