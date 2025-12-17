d2c3f7069ad21822f55d27a3215d66365ce192f3
Haris Mahmood <hmahmood.bee16@gmail.com>
2025-12-17 17:04:33 +0500

    Remove legacy unity/file_manager/parser/ module (replaced by unity/file_manager/file_parsers/)

--- unity/file_manager/parser/converter/__init__.py
+++
@@ -1,9 +0,0 @@
-from .base import ConversionResult, BaseConverter, DocumentConversionManager
-from .docx_to_pdf import DocxToPdfConverter
-
-__all__ = [
-    "ConversionResult",
-    "BaseConverter",
-    "DocumentConversionManager",
-    "DocxToPdfConverter",
-]
