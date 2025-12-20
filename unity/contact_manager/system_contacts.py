c4a2981e9367cce89ab82c12d4a3d17f9aefda00
djl11 <dan@unify.ai>
2025-12-20 12:52:11 +0000

    Update system_contacts.py to use renamed DEFAULT_* constants
    Replace DUMMY_* imports with DEFAULT_* to align with the standardized
    naming convention in session_details.py.

--- unity/contact_manager/system_contacts.py
+++ unity/contact_manager/system_contacts.py
@@ -6,14 +6,14 @@ import unify
 
 from ..knowledge_manager.types import ColumnType
 from ..session_details import (
-    DUMMY_ASSISTANT_BIO,
-    DUMMY_ASSISTANT_EMAIL,
-    DUMMY_ASSISTANT_FIRST_NAME,
-    DUMMY_ASSISTANT_PHONE,
-    DUMMY_ASSISTANT_SURNAME,
-    DUMMY_USER_EMAIL,
-    DUMMY_USER_FIRST_NAME,
-    DUMMY_USER_SURNAME,
+    DEFAULT_ASSISTANT_BIO,
+    DEFAULT_ASSISTANT_EMAIL,
+    DEFAULT_ASSISTANT_FIRST_NAME,
+    DEFAULT_ASSISTANT_PHONE,
+    DEFAULT_ASSISTANT_SURNAME,
+    DEFAULT_USER_EMAIL,
+    DEFAULT_USER_FIRST_NAME,
+    DEFAULT_USER_SURNAME,
 )
 
 
@@ -79,11 +79,11 @@ def sync_assistant_contact(self, assistant_log) -> None:
         base_fields["response_policy"] = ""
         base_fields.update(
             {
-                "first_name": DUMMY_ASSISTANT_FIRST_NAME,
-                "surname": DUMMY_ASSISTANT_SURNAME,
-                "email_address": DUMMY_ASSISTANT_EMAIL,
-                "phone_number": DUMMY_ASSISTANT_PHONE,
-                "bio": DUMMY_ASSISTANT_BIO,
+                "first_name": DEFAULT_ASSISTANT_FIRST_NAME,
+                "surname": DEFAULT_ASSISTANT_SURNAME,
+                "email_address": DEFAULT_ASSISTANT_EMAIL,
+                "phone_number": DEFAULT_ASSISTANT_PHONE,
+                "bio": DEFAULT_ASSISTANT_BIO,
                 "rolling_summary": None,
             },
         )
@@ -146,9 +146,9 @@ def fetch_user_info(self) -> Dict[str, Any]:
         return user_info
 
     return {
-        "first_name": DUMMY_USER_FIRST_NAME,
-        "last_name": DUMMY_USER_SURNAME,
-        "email": DUMMY_USER_EMAIL,
+        "first_name": DEFAULT_USER_FIRST_NAME,
+        "last_name": DEFAULT_USER_SURNAME,
+        "email": DEFAULT_USER_EMAIL,
     }
 
 
