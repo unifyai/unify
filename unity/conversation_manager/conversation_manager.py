2adbc4bd8b615dfe10d77d1dd228b6b445300d5c
juliagsy <67888047+juliagsy@users.noreply.github.com>
2026-03-06 11:20:05 +0800

    reduced inactivity timeout to 5mins

--- unity/conversation_manager/conversation_manager.py
+++ unity/conversation_manager/conversation_manager.py
@@ -123,7 +123,7 @@ class ConversationManager(metaclass=SingletonABCMeta):
         self.project_name = project_name
 
         # inactivity & shutdown
-        self.inactivity_timeout = 900  # 15 minutes in seconds
+        self.inactivity_timeout = 300  # 5 minutes in seconds
         self.inactivity_check_interval = 30  # seconds
         self.last_activity_time = self.loop.time()
         self.stop = stop
